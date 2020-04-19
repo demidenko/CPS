17c3c9f016f4c4d2b9da0d5157824bc614619c52
demich <devit15@yandex.ru>
2020-04-19 17:34:39 +0700

    news view improved

--- app/src/main/java/com/example/test3/NewsFragment.kt
+++ app/src/main/java/com/example/test3/NewsFragment.kt
@@ -1,16 +1,19 @@
 package com.example.test3
 
 import android.content.Intent
-import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
+import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.core.text.HtmlCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.adapter.FragmentStateAdapter
@@ -19,8 +22,6 @@ import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
-import java.lang.Exception
-import java.net.URI
 
 
 class NewsFragment() : Fragment() {
@@ -63,9 +64,7 @@ class NewsFragment() : Fragment() {
 
 
 
-
-
-        view.findViewById<Button>(R.id.button_reload_cf).apply {
+        view.findViewById<Button>(R.id.button_reload_cf_news).apply {
             setOnClickListener { button -> button as Button
                 button.text = "..."
                 button.isEnabled = false
@@ -121,33 +120,54 @@ class CodeforcesNewsFragment(val address: String) : Fragment() {
     }
 
     private lateinit var recyclerView: RecyclerView
-    private lateinit var viewAdapter: MyAdapter
+    private lateinit var viewAdapter: CodeforcesNewsItemsAdapter
     private lateinit var viewManager: RecyclerView.LayoutManager
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         viewManager = LinearLayoutManager(context)
 
-        viewAdapter = MyAdapter(requireActivity(), arrayListOf())
+        viewAdapter = CodeforcesNewsItemsAdapter(requireActivity(), arrayListOf())
 
         recyclerView = view.findViewById<RecyclerView>(R.id.cf_news_page_recyclerview).apply {
             setHasFixedSize(true)
             layoutManager = viewManager
             adapter = viewAdapter
+            addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
         }
     }
 
     suspend fun reload(){
         readURLData(address)?.let { s ->
-            val res = arrayListOf<Pair<String,Int>>()
+            val res = arrayListOf<ArrayList<String>>()
             var i = 0
             while (true) {
                 i = s.indexOf("<div class=\"topic\"", i + 1)
                 if (i == -1) break
-                val title = s.substring(s.indexOf("<p>", i) + 3, s.indexOf("</p>", i))
+                val title = Html.fromHtml(
+                    s.substring(s.indexOf("<p>", i) + 3, s.indexOf("</p>", i)),
+                    HtmlCompat.FROM_HTML_MODE_LEGACY).toString()
+
                 i = s.indexOf("entry/", i)
-                val id = s.substring(i+6, s.indexOf('"',i)).toInt()
-                res.add(Pair(title,id))
+                val id = s.substring(i+6, s.indexOf('"',i))
+
+                i = s.indexOf("<div class=\"info\"", i)
+                i = s.indexOf("</a>", i)
+                val author = s.substring(s.lastIndexOf('>',i-1)+1,i).trim()
+
+                i = s.indexOf("</span>", i)
+                val time = s.substring(s.lastIndexOf('>',i-1)+1,i)
+
+                i = s.indexOf("<div class=\"roundbox meta\"", i)
+                i = s.indexOf("</span>", i)
+                val rating = s.substring(s.lastIndexOf('>',i-1)+1,i)
+
+                i = s.indexOf("<div class=\"right-meta\">", i)
+                i = s.indexOf("</ul>", i)
+                i = s.lastIndexOf("</a>", i)
+                val comments = s.substring(s.lastIndexOf('>',i-1)+1,i).trim()
+
+                res.add(arrayListOf(id,title,author,time,comments,rating))
             }
             viewAdapter.data = res
             viewAdapter.notifyDataSetChanged()
@@ -156,23 +176,35 @@ class CodeforcesNewsFragment(val address: String) : Fragment() {
 }
 
 
-class MyAdapter(val c: FragmentActivity, var data: ArrayList<Pair<String,Int>>): RecyclerView.Adapter<MyAdapter.MyViewHolder>(){
-    class MyViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView){
-
+class CodeforcesNewsItemsAdapter(private val activity: FragmentActivity, var data: ArrayList<ArrayList<String>>): RecyclerView.Adapter<CodeforcesNewsItemsAdapter.CodeforcesNewsItemViewHolder>(){
+    class CodeforcesNewsItemViewHolder(val view: RelativeLayout) : RecyclerView.ViewHolder(view){
+        val title: TextView = view.findViewById(R.id.news_item_title)
+        val author: TextView = view.findViewById(R.id.news_item_author)
+        val time: TextView = view.findViewById(R.id.news_item_time)
+        val rating: TextView = view.findViewById(R.id.news_item_rating)
+        val comments: TextView = view.findViewById(R.id.news_item_comments)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val textView = LayoutInflater.from(parent.context).inflate(R.layout.cf_news_page_item, parent, false) as TextView
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CodeforcesNewsItemViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.cf_news_page_item, parent, false) as RelativeLayout
 
-        return MyViewHolder(textView)
+        return CodeforcesNewsItemViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.textView.text = data[position].first
-        holder.textView.setOnClickListener {
-            println(data[position].second)
-            c.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://codeforces.com/blog/entry/${data[position].second}")))
-        }
+    override fun onBindViewHolder(holder: CodeforcesNewsItemViewHolder, position: Int) {
+        val info = data[position]
+        holder.view.setOnClickListener { activity.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://codeforces.com/blog/entry/${info[0]}"))) }
+        holder.title.text = info[1]
+
+
+        holder.author.text = info[2]
+        holder.time.text = info[3]
+        holder.comments.text = info[4]
+
+        holder.rating.text = info[5]
+        holder.rating.setTextColor(activity.resources.getColor(
+            if(info[5].startsWith('+')) R.color.blog_rating_positive else R.color.blog_rating_negative,
+            null))
     }
 
     override fun getItemCount() = data.size
