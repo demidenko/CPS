cbdcfc25dc5a1159f6cfc6b7359be296a29cbff2
demich <devit15@yandex.ru>
2020-12-11 15:27:23 +0700

    atcoder/topcoder big view

--- app/src/main/java/com/example/test3/account_view/AtCoderAccountPanel.kt
+++ app/src/main/java/com/example/test3/account_view/AtCoderAccountPanel.kt
@@ -1,7 +1,9 @@
 package com.example.test3.account_view
 
 import android.graphics.Typeface
+import android.widget.TextView
 import com.example.test3.MainActivity
+import com.example.test3.R
 import com.example.test3.account_manager.AtCoderAccountManager
 import com.example.test3.account_manager.NOT_RATED
 import com.example.test3.account_manager.STATUS
@@ -13,17 +15,40 @@ class AtCoderAccountPanel(
 ): AccountPanel(mainActivity, manager)  {
 
     override fun show(info: UserInfo) { info as AtCoderAccountManager.AtCoderUserInfo
-        val color = manager.getColor(info)
+        val color = manager.getColor(info) ?: mainActivity.defaultTextColor
         textMain.text = info.handle
-        textMain.setTextColor(color ?: mainActivity.defaultTextColor)
+        textMain.setTextColor(color)
         textAdditional.text = ""
-        textAdditional.setTextColor(color ?: mainActivity.defaultTextColor)
+        textAdditional.setTextColor(color)
         if(info.status == STATUS.OK){
             textMain.typeface = Typeface.DEFAULT_BOLD
             textAdditional.text = if(info.rating == NOT_RATED) "[not rated]" else "${info.rating}"
         }else{
             textMain.typeface = Typeface.DEFAULT
         }
+    }
+
+    override val bigViewResource = R.layout.fragment_account_view_atcoder
+
+    override fun showBigView(fragment: AccountViewFragment) {
+        val view = fragment.requireView()
+
+        val info = manager.savedInfo as AtCoderAccountManager.AtCoderUserInfo
+        val color = manager.getColor(info) ?: mainActivity.defaultTextColor
+
+        val handleView = view.findViewById<TextView>(R.id.account_view_handle)
+        val ratingView = view.findViewById<TextView>(R.id.account_view_rating)
+
+        handleView.setTextColor(color)
+        ratingView.setTextColor(color)
+        handleView.text = info.handle
+        if(info.status == STATUS.OK){
+            handleView.typeface = Typeface.DEFAULT_BOLD
+            ratingView.text = if(info.rating == NOT_RATED) "[not rated]" else "${info.rating}"
+        }else{
+            handleView.typeface = Typeface.DEFAULT
+        }
+
     }
 
 }
--- app/src/main/java/com/example/test3/account_view/TopCoderAccountPanel.kt
+++ app/src/main/java/com/example/test3/account_view/TopCoderAccountPanel.kt
@@ -1,7 +1,10 @@
 package com.example.test3.account_view
 
 import android.graphics.Typeface
+import android.view.View
+import android.widget.TextView
 import com.example.test3.MainActivity
+import com.example.test3.R
 import com.example.test3.account_manager.NOT_RATED
 import com.example.test3.account_manager.STATUS
 import com.example.test3.account_manager.TopCoderAccountManager
@@ -23,6 +26,44 @@ class TopCoderAccountPanel(
             textAdditional.text = if(info.rating_algorithm == NOT_RATED) "[not rated]" else "${info.rating_algorithm}"
         }else{
             textMain.typeface = Typeface.DEFAULT
+        }
+    }
+
+    override val bigViewResource = R.layout.fragment_account_view_topcoder
+
+    override fun showBigView(fragment: AccountViewFragment) {
+        val view = fragment.requireView()
+
+        val info = manager.savedInfo as TopCoderAccountManager.TopCoderUserInfo
+        val color = manager.getColor(info) ?: mainActivity.defaultTextColor
+
+        val handleView = view.findViewById<TextView>(R.id.account_view_handle)
+        val ratingView = view.findViewById<TextView>(R.id.account_view_rating)
+        val marathonRatingView = view.findViewById<TextView>(R.id.account_view_tc_marathon)
+
+        handleView.setTextColor(color)
+        ratingView.setTextColor(color)
+        handleView.text = info.handle
+        if(info.status == STATUS.OK){
+            handleView.typeface = Typeface.DEFAULT_BOLD
+            ratingView.text = if(info.rating_algorithm == NOT_RATED) "[not rated]" else "${info.rating_algorithm}"
+        }else{
+            handleView.typeface = Typeface.DEFAULT
+        }
+
+        marathonRatingView.apply {
+            val marathonRatingViewTitle = view.findViewById<TextView>(R.id.account_view_tc_marathon_title)
+            if(info.rating_marathon == NOT_RATED){
+                visibility = View.GONE
+                marathonRatingViewTitle.visibility = View.GONE
+            }else {
+                visibility = View.VISIBLE
+                marathonRatingViewTitle.visibility = View.VISIBLE
+
+                val marathonColor = TopCoderAccountManager.getHandleColor(info.rating_marathon).getARGB(TopCoderAccountManager.Companion)
+                setTextColor(marathonColor)
+                text = "${info.rating_marathon}"
+            }
         }
     }
 
