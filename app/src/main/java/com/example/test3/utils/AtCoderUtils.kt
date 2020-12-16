f849d0295ad7dc3cd07695983de906b73e55ec25
demich <devit15@yandex.ru>
2020-12-16 14:09:30 +0700

    atcoder get rating changes

--- app/src/main/java/com/example/test3/utils/AtCoderUtils.kt
+++ app/src/main/java/com/example/test3/utils/AtCoderUtils.kt
@@ -3,6 +3,8 @@ package com.example.test3.utils
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Response
@@ -12,6 +14,16 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 import java.io.IOException
 
+
+@Serializable
+data class AtCoderRatingChange(
+    val NewRating: Int,
+    val OldRating: Int,
+    val Place: Int,
+    val EndTime: Long,
+    val ContestName: String
+    //val StandingsUrl: String
+)
 
 object AtCoderAPI {
     interface WEB {
@@ -46,6 +58,21 @@ object AtCoderAPI {
             atcoderWEB.getRankingSearch(str).execute()
         }catch (e: IOException){
             null
+        }
+    }
+
+    suspend fun getRatingChanges(handle: String): List<AtCoderRatingChange>? = withContext(Dispatchers.IO){
+        try {
+            val response = atcoderWEB.getUser(handle).execute() ?: return@withContext null
+            if(!response.isSuccessful) return@withContext null
+            val s = response.body()?.string() ?: return@withContext null
+            val i = s.lastIndexOf("<script>rating_history=[{")
+            if(i==-1) return@withContext null
+            val j = s.indexOf("}];</script>", i)
+            val str = s.substring(s.indexOf('=',i)+1, j+2)
+            return@withContext jsonCPS.decodeFromString<List<AtCoderRatingChange>>(str)
+        }catch (e: IOException){
+            return@withContext null
         }
     }
 }
