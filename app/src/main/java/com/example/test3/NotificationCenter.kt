aa96ee57411e41bb645f12fb9057b27160b5d85b
demich <devit15@yandex.ru>
2021-02-27 11:54:41 +0700

    NotificationCenter code refactor

--- app/src/main/java/com/example/test3/NotificationCenter.kt
+++ app/src/main/java/com/example/test3/NotificationCenter.kt
@@ -21,37 +21,47 @@ fun notificationBuilder(context: Context, channel: NotificationChannelLazy): Not
 fun NotificationCompat.Builder.setBigContent(str: CharSequence) = setContentText(str).setStyle(NotificationCompat.BigTextStyle().bigText(str))
 
 object NotificationChannels {
-    const val IMPORTANCE_MIN = 1
-    const val IMPORTANCE_DEFAULT = 3
 
     //codeforces
     private val group_codeforces by lazy { NotificationChannelGroupLazy("codeforces", "CodeForces") }
-    val codeforces_contest_watcher by lazy { NotificationChannelLazy("cf_contest_watcher", "Contest watch", IMPORTANCE_DEFAULT, group_codeforces) }
-    val codeforces_rating_changes by lazy { NotificationChannelLazy("cf_rating_changes", "Rating changes", IMPORTANCE_DEFAULT, group_codeforces) }
-    val codeforces_contribution_changes by lazy { NotificationChannelLazy("cf_contribution_changes", "Contribution changes", IMPORTANCE_MIN, group_codeforces) }
-    val codeforces_follow_new_blog by lazy { NotificationChannelLazy("cf_follow_new_blog", "Followed blogs", IMPORTANCE_DEFAULT, group_codeforces) }
+    val codeforces_contest_watcher by lazy { NotificationChannelLazy("cf_contest_watcher", "Contest watch", Importance.DEFAULT, group_codeforces) }
+    val codeforces_rating_changes by lazy { NotificationChannelLazy("cf_rating_changes", "Rating changes", Importance.DEFAULT, group_codeforces) }
+    val codeforces_contribution_changes by lazy { NotificationChannelLazy("cf_contribution_changes", "Contribution changes", Importance.MIN, group_codeforces) }
+    val codeforces_follow_new_blog by lazy { NotificationChannelLazy("cf_follow_new_blog", "Followed blogs", Importance.DEFAULT, group_codeforces) }
 
     //atcoder
     private val group_atcoder by lazy { NotificationChannelGroupLazy("atcoder", "AtCoder") }
-    val atcoder_rating_changes by lazy { NotificationChannelLazy("atcoder_rating_changes", "Rating changes", IMPORTANCE_DEFAULT, group_atcoder) }
+    val atcoder_rating_changes by lazy { NotificationChannelLazy("atcoder_rating_changes", "Rating changes", Importance.DEFAULT, group_atcoder) }
 
     //project euler
     private val group_project_euler by lazy { NotificationChannelGroupLazy("project_euler", "Project Euler") }
-    val project_euler_news by lazy { NotificationChannelLazy("pe_news", "Recent Problems", IMPORTANCE_DEFAULT, group_project_euler) }
-    val project_euler_problems by lazy { NotificationChannelLazy("pe_problems", "News", IMPORTANCE_DEFAULT, group_project_euler) }
+    val project_euler_news by lazy { NotificationChannelLazy("pe_news", "Recent Problems", Importance.DEFAULT, group_project_euler) }
+    val project_euler_problems by lazy { NotificationChannelLazy("pe_problems", "News", Importance.DEFAULT, group_project_euler) }
 
     //acmp
     private val group_acmp by lazy { NotificationChannelGroupLazy("acmp", "ACMP") }
-    val acmp_news by lazy { NotificationChannelLazy("acmp_news", "News", IMPORTANCE_DEFAULT, group_acmp) }
+    val acmp_news by lazy { NotificationChannelLazy("acmp_news", "News", Importance.DEFAULT, group_acmp) }
 
     //zaoch
     private val group_zaoch by lazy { NotificationChannelGroupLazy("zaoch", "olympiads.ru/zaoch") }
-    val olympiads_zaoch_news by lazy { NotificationChannelLazy("olympiads_zaoch_news", "News", IMPORTANCE_DEFAULT, group_zaoch) }
+    val olympiads_zaoch_news by lazy { NotificationChannelLazy("olympiads_zaoch_news", "News", Importance.DEFAULT, group_zaoch) }
 
     //test
     private val group_test by lazy { NotificationChannelGroupLazy("test", "Test group") }
-    val test by lazy { NotificationChannelLazy("test", "test channel", IMPORTANCE_DEFAULT, group_test) }
+    val test by lazy { NotificationChannelLazy("test", "test channel", Importance.DEFAULT, group_test) }
 
+
+    enum class Importance {
+        MIN,
+        DEFAULT;
+
+        @RequiresApi(Build.VERSION_CODES.N)
+        fun convert(): Int =
+            when(this){
+                DEFAULT -> NotificationManager.IMPORTANCE_DEFAULT
+                MIN -> NotificationManager.IMPORTANCE_MIN
+            }
+    }
 }
 
 class NotificationChannelGroupLazy(private val id: String, val name: String){
@@ -69,15 +79,15 @@ class NotificationChannelGroupLazy(private val id: String, val name: String){
 class NotificationChannelLazy(
     private val id: String,
     val name: String,
-    val importance: Int,
-    val groupCreator: NotificationChannelGroupLazy
+    private val importance: NotificationChannels.Importance,
+    private val groupCreator: NotificationChannelGroupLazy
 ){
     private var created = false
     fun getID(context: Context): String {
         if(!created){
             if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O){
                 val m = NotificationManagerCompat.from(context)
-                val channel = NotificationChannel(id, name, convertImportance()).apply {
+                val channel = NotificationChannel(id, name, importance.convert()).apply {
                     group = groupCreator.getID(m)
                 }
                 m.createNotificationChannel(channel)
@@ -85,15 +95,6 @@ class NotificationChannelLazy(
             created = true
         }
         return id
-    }
-
-    @RequiresApi(Build.VERSION_CODES.N)
-    fun convertImportance(): Int {
-        return when (importance){
-            NotificationChannels.IMPORTANCE_DEFAULT -> NotificationManager.IMPORTANCE_DEFAULT
-            NotificationChannels.IMPORTANCE_MIN -> NotificationManager.IMPORTANCE_MIN
-            else -> NotificationManager.IMPORTANCE_UNSPECIFIED
-        }
     }
 }
 
@@ -156,10 +157,11 @@ object NotificationColors {
 fun makeSimpleNotification(context: Context, id: Int, title: String, content: String, silent: Boolean = true){
     val n = notificationBuilder(context, NotificationChannels.test).apply {
         setSmallIcon(R.drawable.ic_news)
-        setShowWhen(true)
         setContentTitle(title)
         setContentText(content)
         if(silent) setNotificationSilent()
+        setShowWhen(true)
+        setWhen(System.currentTimeMillis())
     }
     NotificationManagerCompat.from(context).notify(id, n.build())
 }
