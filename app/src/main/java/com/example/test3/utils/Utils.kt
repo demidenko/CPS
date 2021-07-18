fcdf7163517c605a7acd87e28d42c94a941a1c48
demich <devit15@yandex.ru>
2021-07-18 16:23:08 +0700

    DataStore Item helper class generalized

--- app/src/main/java/com/example/test3/utils/Utils.kt
+++ app/src/main/java/com/example/test3/utils/Utils.kt
@@ -89,61 +89,57 @@ class MutableSetLiveSize<T>() {
 
 open class CPSDataStore(protected val dataStore: DataStore<Preferences>) {
 
-    inner class Item<T> (
-        val key: Preferences.Key<T>,
-        private val defaultValue: T
-    ) {
-        val flow: Flow<T> = dataStore.data.map { it[key] ?: defaultValue }.distinctUntilChanged()
+    abstract inner class CPSDataStoreItem<T, S> {
+        abstract val key: Preferences.Key<S>
+        protected abstract fun fromPrefs(s: S?): T
+        //TODO [toPrefs(t: T!!): S] in 1.6??
+        protected abstract fun toPrefs(t: T): S
+
+        val flow: Flow<T> = dataStore.data.map { fromPrefs(it[key]) }.distinctUntilChanged()
 
         //getter
-        suspend inline operator fun invoke(): T = flow.first()
+        suspend operator fun invoke(): T = fromPrefs(dataStore.data.first()[key])
 
         //setter
         suspend operator fun invoke(newValue: T) {
-            dataStore.edit { it[key] = newValue }
+            dataStore.edit { prefs ->
+                newValue?.let { prefs[key] = toPrefs(it) } ?: prefs.remove(key)
+            }
         }
     }
 
+    inner class Item<T> (
+        override val key: Preferences.Key<T>,
+        private val defaultValue: T
+    ): CPSDataStoreItem<T, T>() {
+        override fun fromPrefs(s: T?): T = s ?: defaultValue
+        override fun toPrefs(t: T): T = t
+    }
+
     inner class ItemNullable<T> (
-        val key: Preferences.Key<T>
-    ) {
-        val flow: Flow<T?> = dataStore.data.map { it[key] }.distinctUntilChanged()
-
-        //getter
-        suspend inline operator fun invoke(): T? = flow.first()
-
-        //setter
-        suspend operator fun invoke(newValue: T?) {
-            dataStore.edit { prefs ->
-                newValue?.let { prefs[key] = it } ?: prefs.remove(key)
-            }
-        }
+        override val key: Preferences.Key<T>
+    ): CPSDataStoreItem<T?, T>() {
+        override fun fromPrefs(s: T?): T? = s
+        override fun toPrefs(t: T?): T = t!!
     }
 
     inner class ItemEnum<T: Enum<T>> (
         name: String,
         private val clazz: Class<T>,
         private val defaultValueCallback: () -> T
-    ) {
+    ): CPSDataStoreItem<T, String>() {
         constructor(name: String, defaultValue: T): this(
             name, defaultValue.javaClass, defaultValueCallback = { defaultValue }
         )
+        override val key = stringPreferencesKey(name)
 
-        val key = stringPreferencesKey(name)
-
-        val flow: Flow<T> = dataStore.data.map {
-            it[key]?.let { str ->
+        override fun fromPrefs(s: String?): T {
+            return s?.let { str ->
                 clazz.enumConstants.first { it.name == str }
             } ?: defaultValueCallback()
-        }.distinctUntilChanged()
-
-        //getter
-        suspend inline operator fun invoke(): T = flow.first()
-
-        //setter
-        suspend operator fun invoke(newValue: T) {
-            dataStore.edit { it[key] = newValue.name }
         }
+
+        override fun toPrefs(t: T): String = t.name
     }
 
     inner class ItemStringConvertible<T> (
@@ -151,18 +147,10 @@ open class CPSDataStore(protected val dataStore: DataStore<Preferences>) {
         private val defaultValue: T,
         private val encode: (T) -> String,
         private val decode: (String) -> T,
-    ) {
-        val key = stringPreferencesKey(name)
-
-        val flow: Flow<T> = dataStore.data.map { it[key]?.let(decode) ?: defaultValue }.distinctUntilChanged()
-
-        //getter
-        suspend inline operator fun invoke(): T = flow.first()
-
-        //setter
-        suspend operator fun invoke(newValue: T) {
-            dataStore.edit { it[key] = encode(newValue) }
-        }
+    ): CPSDataStoreItem<T, String>() {
+        override val key = stringPreferencesKey(name)
+        override fun fromPrefs(s: String?): T = s?.let(decode) ?: defaultValue
+        override fun toPrefs(t: T): String = encode(t)
     }
 
     inline fun<reified T> Json.itemStringConvertible(name: String, defaultValue: T) =
