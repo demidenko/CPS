9cb40fa8cbab6f08f14f769e87d8860be8bbe9e4
demich <devit15@yandex.ru>
2021-07-18 16:26:53 +0700

    CPSDataStore to own file

--- app/src/main/java/com/example/test3/utils/Utils.kt
+++ app/src/main/java/com/example/test3/utils/Utils.kt
@@ -5,10 +5,6 @@ import android.text.Html
 import android.text.Spanned
 import android.widget.EditText
 import androidx.core.text.HtmlCompat
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -19,8 +15,6 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType
 import okhttp3.OkHttpClient
@@ -86,82 +80,6 @@ class MutableSetLiveSize<T>() {
         size.value = 0
     }
 }
-
-open class CPSDataStore(protected val dataStore: DataStore<Preferences>) {
-
-    abstract inner class CPSDataStoreItem<T, S> {
-        abstract val key: Preferences.Key<S>
-        protected abstract fun fromPrefs(s: S?): T
-        //TODO [toPrefs(t: T!!): S] in 1.6??
-        protected abstract fun toPrefs(t: T): S
-
-        val flow: Flow<T> = dataStore.data.map { fromPrefs(it[key]) }.distinctUntilChanged()
-
-        //getter
-        suspend operator fun invoke(): T = fromPrefs(dataStore.data.first()[key])
-
-        //setter
-        suspend operator fun invoke(newValue: T) {
-            dataStore.edit { prefs ->
-                newValue?.let { prefs[key] = toPrefs(it) } ?: prefs.remove(key)
-            }
-        }
-    }
-
-    inner class Item<T> (
-        override val key: Preferences.Key<T>,
-        private val defaultValue: T
-    ): CPSDataStoreItem<T, T>() {
-        override fun fromPrefs(s: T?): T = s ?: defaultValue
-        override fun toPrefs(t: T): T = t
-    }
-
-    inner class ItemNullable<T> (
-        override val key: Preferences.Key<T>
-    ): CPSDataStoreItem<T?, T>() {
-        override fun fromPrefs(s: T?): T? = s
-        override fun toPrefs(t: T?): T = t!!
-    }
-
-    inner class ItemEnum<T: Enum<T>> (
-        name: String,
-        private val clazz: Class<T>,
-        private val defaultValueCallback: () -> T
-    ): CPSDataStoreItem<T, String>() {
-        constructor(name: String, defaultValue: T): this(
-            name, defaultValue.javaClass, defaultValueCallback = { defaultValue }
-        )
-        override val key = stringPreferencesKey(name)
-
-        override fun fromPrefs(s: String?): T {
-            return s?.let { str ->
-                clazz.enumConstants.first { it.name == str }
-            } ?: defaultValueCallback()
-        }
-
-        override fun toPrefs(t: T): String = t.name
-    }
-
-    inner class ItemStringConvertible<T> (
-        name: String,
-        private val defaultValue: T,
-        private val encode: (T) -> String,
-        private val decode: (String) -> T,
-    ): CPSDataStoreItem<T, String>() {
-        override val key = stringPreferencesKey(name)
-        override fun fromPrefs(s: String?): T = s?.let(decode) ?: defaultValue
-        override fun toPrefs(t: T): String = encode(t)
-    }
-
-    inline fun<reified T> Json.itemStringConvertible(name: String, defaultValue: T) =
-        ItemStringConvertible(
-            name = name,
-            defaultValue = defaultValue,
-            encode = ::encodeToString,
-            decode = ::decodeFromString
-        )
-}
-
 
 
 enum class LoadingState {
