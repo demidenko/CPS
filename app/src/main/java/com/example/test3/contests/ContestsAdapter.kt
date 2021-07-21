6da8e82e5fae2aeed8e8390cc909f7d0ae55412a
demich <devit15@yandex.ru>
2021-07-21 10:31:48 +0700

    additional to separated view

--- app/src/main/java/com/example/test3/contests/ContestsAdapter.kt
+++ app/src/main/java/com/example/test3/contests/ContestsAdapter.kt
@@ -1,10 +1,6 @@
 package com.example.test3.contests
 
-import android.content.Context
-import android.text.Spannable
-import android.text.SpannableStringBuilder
 import android.text.format.DateFormat
-import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -140,8 +136,8 @@ abstract class ContestViewHolder(protected val view: ConstraintLayout): Recycler
     companion object {
         fun makeDate(timeSeconds: Long): CharSequence = DateFormat.format("dd.MM E HH:mm", TimeUnit.SECONDS.toMillis(timeSeconds))
 
-        fun muteTrailingBrackets(title: String, context: Context): CharSequence {
-            if (title.isEmpty() || title.last()!=')') return title
+        fun cutTrailingBrackets(title: String): Pair<String, String> {
+            if (title.isEmpty() || title.last()!=')') return title to ""
             var i = title.length-2
             var ballance = 1
             while (ballance>0 && i>0) {
@@ -152,22 +148,24 @@ abstract class ContestViewHolder(protected val view: ConstraintLayout): Recycler
                 if (ballance == 0) break
                 --i
             }
-            if (ballance!=0) return title
-            return SpannableStringBuilder(title).apply {
-                setSpan(ForegroundColorSpan(getColorFromResource(context,R.color.textColorAdditional)), i, title.length, Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
-            }
+            if (ballance!=0) return title to ""
+            return title.substring(0, i) to title.substring(i)
         }
     }
 }
 
 class ContestItemViewHolder(view: ConstraintLayout): ContestViewHolder(view) {
     private val title: TextView = view.findViewById(R.id.contests_list_item_title)
+    private val titleAdditional: TextView = view.findViewById(R.id.contests_list_item_title_additional)
     private val date: TextView = view.findViewById(R.id.contests_list_item_date)
     private val counterTextView: TextView = view.findViewById(R.id.contests_list_item_counter)
     private val icon: ImageView = view.findViewById(R.id.contests_list_item_icon)
 
     override fun applyContest(contest: Contest) {
-        title.text = muteTrailingBrackets(contest.title, view.context)
+        with(cutTrailingBrackets(contest.title)) {
+            title.text = first
+            titleAdditional.text = second
+        }
         icon.setImageResource(contest.platform.getIcon())
         view.setOnClickListener { contest.link?.let { url -> it.context.startActivity(makeIntentOpenUrl(url)) } }
     }
