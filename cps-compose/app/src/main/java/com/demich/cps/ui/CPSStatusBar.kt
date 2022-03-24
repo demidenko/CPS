fafd4981a41366f31a1b9a5c9c086f7ea8c7f72b
demidenko <devit15@yandex.ru>
2022-03-25 02:29:03 +0700

    std solution (how to google it??)

--- cps-compose/app/src/main/java/com/demich/cps/ui/CPSStatusBar.kt
+++ cps-compose/app/src/main/java/com/demich/cps/ui/CPSStatusBar.kt
@@ -6,10 +6,9 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.lerp
 import com.demich.cps.accounts.allAccountManagers
 import com.demich.cps.accounts.managers.*
 import com.demich.cps.ui.theme.cpsColors
@@ -99,17 +98,8 @@ private fun ColorizeStatusBar(
         targetValue = color,
         animationSpec = tween(buttonOnOffDurationMillis)
     )
-    val mixedColor by remember(offColor) {
-        fun point(l: Float, r: Float): Float = (r - l) * koef + l
-        derivedStateOf {
-            val r = point(offColor.red, statusBarColor.red)
-            val g = point(offColor.green, statusBarColor.green)
-            val b = point(offColor.blue, statusBarColor.blue)
-            Color(r, g, b)
-        }
-    }
     systemUiController.setStatusBarColor(
-        color = mixedColor,
+        color = lerp(offColor, statusBarColor, koef),
         darkIcons = MaterialTheme.colors.isLight
     )
 }
