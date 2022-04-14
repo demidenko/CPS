882d31c181305b2df3df2550131e417970b39693
demidenko <devit15@yandex.ru>
2022-04-14 23:23:42 +0700

    progress bar fix shadow + round

--- cps-compose/app/src/main/java/com/demich/cps/ui/bottomprogressbar/CPSBottomProgressBar.kt
+++ cps-compose/app/src/main/java/com/demich/cps/ui/bottomprogressbar/CPSBottomProgressBar.kt
@@ -1,18 +1,22 @@
 package com.demich.cps.ui.bottomprogressbar
 
 import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.LinearProgressIndicator
-import androidx.compose.material.Surface
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ProgressIndicatorDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -43,30 +47,56 @@ fun CPSBottomProgressBar(
     progressBarInfo: ProgressBarInfo,
     modifier: Modifier = Modifier
 ) {
-    if (progressBarInfo.total > 0) Surface(
-        modifier = modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(8.dp),
-        color = cpsColors.backgroundNavigation.copy(alpha = 0.5f),
-        elevation = 5.dp
-    ) {
+    if (progressBarInfo.total > 0) {
+        val progress by animateFloatAsState(targetValue = progressBarInfo.fraction)
         Row(
             verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.padding(vertical = 4.dp)
+            modifier = modifier
+                .fillMaxWidth()
+                .background(
+                    color = cpsColors.backgroundNavigation.copy(alpha = 0.75f),
+                    shape = RoundedCornerShape(8.dp)
+                )
+                .padding(vertical = 4.dp)
         ) {
             MonospacedText(
                 text = progressBarInfo.title,
                 fontSize = 13.sp,
                 color = cpsColors.textColorAdditional,
                 textAlign = TextAlign.Center,
-                modifier = Modifier.weight(3f).padding(horizontal = 5.dp)
+                modifier = Modifier.padding(horizontal = 10.dp).weight(3f)
             )
-            val progress by animateFloatAsState(targetValue = progressBarInfo.fraction)
-            LinearProgressIndicator(
+            LinearProgressIndicatorRounded(
                 progress = progress,
-                modifier = Modifier
-                    .weight(5f)
-                    .padding(end = 10.dp)
+                modifier = Modifier.padding(end = 10.dp).weight(5f)
             )
         }
+    }
+}
+
+@Composable
+fun LinearProgressIndicatorRounded(
+    progress: Float,
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colors.primary,
+    backgroundColor: Color = color.copy(alpha = ProgressIndicatorDefaults.IndicatorBackgroundOpacity)
+) {
+    Canvas(
+        modifier = modifier
+            .height(4.dp)
+            .width(240.dp)
+    ) {
+        drawRoundRect(
+            color = backgroundColor,
+            topLeft = Offset.Zero,
+            size = size,
+            cornerRadius = CornerRadius(size.height)
+        )
+        drawRoundRect(
+            color = color,
+            topLeft = Offset.Zero,
+            size = Size(height = size.height, width = progress.coerceIn(0f, 1f) * size.width),
+            cornerRadius = CornerRadius(size.height)
+        )
     }
 }
