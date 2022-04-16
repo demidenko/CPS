d538cc2242ebe5a2c93404acabeec39db74164df
demidenko <devit15@yandex.ru>
2022-04-16 15:30:44 +0700

    scrollbar as modifier

--- cps-compose/app/src/main/java/com/demich/cps/ui/LazyColumnWithScrollBar.kt
+++ cps-compose/app/src/main/java/com/demich/cps/ui/LazyColumnWithScrollBar.kt
@@ -1,9 +1,6 @@
 package com.demich.cps.ui
 
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.LazyListState
@@ -11,6 +8,7 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
@@ -26,54 +24,47 @@ fun LazyColumnWithScrollBar(
     state: LazyListState = rememberLazyListState(),
     verticalArrangement: Arrangement.Vertical = Arrangement.Top,
     horizontalAlignment: Alignment.Horizontal = Alignment.Start,
-    scrollBarWidth: Dp = 5.dp,
     content: LazyListScope.() -> Unit
 ) {
-    Box(
+    LazyColumn(
         modifier = modifier
-    ) {
-        LazyColumn(
-            state = state,
-            verticalArrangement = verticalArrangement,
-            horizontalAlignment = horizontalAlignment,
-            content = content
-        )
-        VerticalScrollBarByCanvas(
-            listState = state,
-            scrollBarWidth = scrollBarWidth,
-            modifier = Modifier.align(Alignment.CenterEnd)
-        )
-    }
+            .lazyColumnScrollBar(
+                listState = state,
+                scrollBarColor = cpsColors.textColor.copy(alpha = 0.5f),
+                scrollBarWidth = 5.dp
+            ),
+        state = state,
+        verticalArrangement = verticalArrangement,
+        horizontalAlignment = horizontalAlignment,
+        content = content
+    )
 }
 
-@Composable
-private fun VerticalScrollBarByCanvas(
+fun Modifier.lazyColumnScrollBar(
     listState: LazyListState,
-    modifier: Modifier = Modifier,
+    scrollBarColor: Color,
     scrollBarWidth: Dp,
-    scrollBarColor: Color = cpsColors.textColor.copy(alpha = 0.5f),
     minimumScrollBarHeight: Dp = 10.dp
-) {
+): Modifier = this.drawWithContent {
+    drawContent()
     val info = listState.layoutInfo
     val count = info.visibleItemsInfo.size
     if (count > 0) {
         val windowSize = info.viewportEndOffset - info.viewportStartOffset
         val visibleItemsSize = info.visibleItemsInfo.sumOf { it.size }
         if (windowSize < visibleItemsSize) {
-            Canvas(modifier = modifier.size(width = scrollBarWidth, height = windowSize.dp)) {
-                val itemSize: Float = visibleItemsSize.toFloat() / count
-                val totalSize: Float = info.totalItemsCount * itemSize
-                val beforeSize: Float = listState.firstVisibleItemIndex * itemSize - info.visibleItemsInfo.first().offset
-                val h = windowSize / totalSize * windowSize //real height of bar
-                val scrollBarHeight = max(h, minimumScrollBarHeight.toPx())
-                val offsetY = beforeSize / totalSize * windowSize - beforeSize / (totalSize - windowSize) * (scrollBarHeight - h)
-                drawRoundRect(
-                    color = scrollBarColor,
-                    topLeft = Offset(x = 0f, y = offsetY),
-                    size = Size(width = scrollBarWidth.toPx(), height = scrollBarHeight),
-                    cornerRadius = CornerRadius(4.dp.toPx())
-                )
-            }
+            val itemSize: Float = visibleItemsSize.toFloat() / count
+            val totalSize: Float = info.totalItemsCount * itemSize
+            val beforeSize: Float = listState.firstVisibleItemIndex * itemSize - info.visibleItemsInfo.first().offset
+            val h = windowSize / totalSize * windowSize //real height of bar
+            val scrollBarHeight = max(h, minimumScrollBarHeight.toPx())
+            val offsetY = beforeSize / totalSize * windowSize - beforeSize / (totalSize - windowSize) * (scrollBarHeight - h)
+            drawRoundRect(
+                color = scrollBarColor,
+                topLeft = Offset(x = size.width - scrollBarWidth.toPx(), y = offsetY),
+                size = Size(width = scrollBarWidth.toPx(), height = scrollBarHeight),
+                cornerRadius = CornerRadius(4.dp.toPx())
+            )
         }
     }
 }
