80796f7276b223e11145f9e0671c2a5547b2dd4c
demidenko <devit15@yandex.ru>
2022-05-18 00:29:33 +0700

    deoversolving

--- cps-compose/app/src/main/java/com/demich/cps/news/codeforces/CodeforcesRecentBlogEntries.kt
+++ cps-compose/app/src/main/java/com/demich/cps/news/codeforces/CodeforcesRecentBlogEntries.kt
@@ -1,8 +1,6 @@
 package com.demich.cps.news.codeforces
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Divider
@@ -11,14 +9,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.Dimension
 import com.demich.cps.ui.EmptyListMessageBox
 import com.demich.cps.utils.codeforces.CodeforcesBlogEntry
 import com.demich.cps.utils.codeforces.CodeforcesRecentAction
@@ -122,39 +119,27 @@ private fun RecentBlogEntryFooter(
     fontSize: TextUnit,
     iconSize: TextUnit
 ) {
-    ConstraintLayout(modifier = modifier) {
-        val (author, comments) = createRefs()
-
+    Row(modifier = modifier.fillMaxWidth()) {
         Text(
             text = authorHandle,
             fontSize = fontSize,
             maxLines = 1,
-            modifier = Modifier.constrainAs(author) {
-                top.linkTo(parent.top)
-                start.linkTo(parent.start)
-                width = Dimension.wrapContent
-            }
         )
-
         if (commentators.isNotEmpty()) {
-            //TODO: row doesn't change width on new data [only release build] (God bless ConstraintLayout-Compose)
-            CommentsRow(
-                text = commentators,
-                fontSize = fontSize,
-                iconSize = iconSize,
-                spaceSize = 1.dp,
-                modifier = Modifier.constrainAs(comments) {
-                    top.linkTo(parent.top)
-                    linkTo(
-                        start = author.end,
-                        end = parent.end,
-                        bias = 1f,
-                        startMargin = 10.dp
-                    )
-                    width = Dimension.preferredWrapContent
-                }
-            )
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 10.dp)
+                    .fillMaxWidth()
+            ) {
+                CommentsRow(
+                    text = commentators,
+                    fontSize = fontSize,
+                    iconSize = iconSize,
+                    spaceSize = 1.dp,
+                    modifier = Modifier.align(Alignment.TopEnd)
+                )
+            }
         }
-
     }
 }
