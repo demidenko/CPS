5a3b04bb9d1352512421b5949ed96260ecacd13e
demidenko <devit15@yandex.ru>
2022-05-31 23:56:37 +0700

    follow list to top on adding

--- cps-compose/app/src/main/java/com/demich/cps/news/follow/NewsFollowList.kt
+++ cps-compose/app/src/main/java/com/demich/cps/news/follow/NewsFollowList.kt
@@ -1,7 +1,9 @@
 package com.demich.cps.news.follow
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Divider
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -23,32 +25,54 @@ import kotlinx.coroutines.flow.map
 fun NewsFollowList() {
     val context = context
 
+    val manager = remember { CodeforcesAccountManager(context) }
+    val currentTime by collectCurrentTimeEachMinute()
+    CompositionLocalProvider(
+        LocalCodeforcesAccountManager provides manager,
+        LocalCurrentTime provides currentTime,
+    ) {
+        NewsFollowListItems()
+    }
+
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+private fun NewsFollowListItems() {
+    val context = context
+
     val userBlogsState = rememberCollect {
         context.followListDao.flowOfAll().map {
             it.sortedByDescending { it.id }
         }
     }
 
-    val manager = remember { CodeforcesAccountManager(context) }
-    val currentTime by collectCurrentTimeEachMinute()
-    CompositionLocalProvider(
-        LocalCodeforcesAccountManager provides manager,
-        LocalCurrentTime provides currentTime
-    ) {
-        LazyColumnWithScrollBar {
-            itemsNotEmpty(userBlogsState.value) {
-                NewsFollowListItem(
-                    userInfo = it.userInfo,
-                    blogEntriesCount = it.blogEntries?.size,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 8.dp, vertical = 5.dp)
-                )
-                Divider()
-            }
-        }
+    val listState = rememberLazyListState()
+    val firstId by remember {
+        derivedStateOf { userBlogsState.value.firstOrNull()?.id }
+    }
+    LaunchedEffect(firstId) {
+        listState.animateScrollToItem(index = 0)
     }
 
+    LazyColumnWithScrollBar(
+        state = listState
+    ) {
+        itemsNotEmpty(
+            items = userBlogsState.value,
+            key = { it.id }
+        ) {
+            NewsFollowListItem(
+                userInfo = it.userInfo,
+                blogEntriesCount = it.blogEntries?.size,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp, vertical = 5.dp)
+                    .animateItemPlacement()
+            )
+            Divider()
+        }
+    }
 }
 
 fun newsFollowListBottomBarBuilder(
