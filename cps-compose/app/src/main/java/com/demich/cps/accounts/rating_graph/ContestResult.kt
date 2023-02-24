1de40fc11622116ba3c07b1c172ed1c963e07821
demidenko <devit15@yandex.ru>
2023-02-24 18:07:07 +0700

    preview tests

--- cps-compose/app/src/main/java/com/demich/cps/accounts/rating_graph/ContestResult.kt
+++ cps-compose/app/src/main/java/com/demich/cps/accounts/rating_graph/ContestResult.kt
@@ -1,5 +1,7 @@
 package com.demich.cps.accounts.rating_graph
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
@@ -9,32 +11,30 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.demich.cps.accounts.managers.HandleColor
 import com.demich.cps.accounts.managers.RatedAccountManager
 import com.demich.cps.accounts.managers.RatedUserInfo
 import com.demich.cps.accounts.managers.RatingChange
+import com.demich.cps.ui.theme.CPSTheme
 import com.demich.cps.ui.theme.cpsColors
 import com.demich.cps.utils.format
 import com.demich.cps.utils.toSignedString
+import kotlinx.datetime.Instant
 
 @Composable
 internal fun ContestResult(
     ratingChange: RatingChange,
     manager: RatedAccountManager<out RatedUserInfo>,
     rectangles: RatingGraphRectangles,
-    modifier: Modifier = Modifier,
-    titleFontSize: TextUnit = 16.sp,
-    subTitleFontSize: TextUnit = 12.sp,
-    ratingFontSize: TextUnit = 30.sp
+    modifier: Modifier = Modifier
 ) {
     ContestResult(
         ratingChange = ratingChange,
         ratingColor = manager.colorFor(handleColor = rectangles.getHandleColor(ratingChange.toPoint())),
-        titleFontSize = titleFontSize,
-        subTitleFontSize = subTitleFontSize,
-        ratingFontSize = ratingFontSize,
         modifier = modifier
     )
 }
@@ -43,10 +43,10 @@ internal fun ContestResult(
 private fun ContestResult(
     ratingChange: RatingChange,
     ratingColor: Color,
-    titleFontSize: TextUnit,
-    subTitleFontSize: TextUnit,
-    ratingFontSize: TextUnit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    titleFontSize: TextUnit = 16.sp,
+    subTitleFontSize: TextUnit = 12.sp,
+    ratingFontSize: TextUnit = 30.sp
 ) {
     Row(
         modifier = modifier,
@@ -86,6 +86,64 @@ private fun ContestResult(
                     color = if (change < 0) cpsColors.error else cpsColors.success,
                 )
             }
+        }
+    }
+}
+
+
+@Composable
+private fun ContestResultTest(
+    hasRank: Boolean,
+    change: Int?,
+    longTitle: Boolean = false,
+    handleColor: HandleColor = HandleColor.ORANGE
+) {
+    val rating = 2150
+    ContestResult(
+        ratingChange = RatingChange(
+            title = "Contest " + "very long ".repeat(if (longTitle) 10 else 0) + "title",
+            date = Instant.fromEpochSeconds(1e9.toLong()),
+            rating = rating,
+            rank = 345.takeIf { hasRank },
+            oldRating = when  {
+                change == null -> null
+                change > 0 -> rating - 150
+                change < 0 -> rating + 150
+                else -> rating
+            }
+        ),
+        ratingColor = cpsColors.handleColor(handleColor = handleColor),
+        modifier = Modifier
+            .background(cpsColors.backgroundAdditional)
+            .padding(all = 3.dp)
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ContestResults() {
+    CPSTheme {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(5.dp),
+            modifier = Modifier.padding(5.dp)
+        ) {
+            ContestResultTest(
+                hasRank = true,
+                change = +1
+            )
+            ContestResultTest(
+                hasRank = false,
+                change = -1
+            )
+            ContestResultTest(
+                hasRank = true,
+                change = null
+            )
+            ContestResultTest(
+                hasRank = true,
+                change = 0,
+                longTitle = true
+            )
         }
     }
 }
