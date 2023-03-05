ad230c58939a53bda969d3c2607c0f55d0061d01
demidenko <devit15@yandex.ru>
2023-03-05 15:52:01 +0700

    filter cf before map

--- cps-compose/app/src/main/java/com/demich/cps/contests/loaders/CodeforcesContestsLoader.kt
+++ cps-compose/app/src/main/java/com/demich/cps/contests/loaders/CodeforcesContestsLoader.kt
@@ -1,11 +1,16 @@
 package com.demich.cps.contests.loaders
 
 import com.demich.cps.contests.Contest
+import com.demich.cps.contests.settings.ContestDateConstraints
 import com.demich.cps.utils.codeforces.CodeforcesApi
 
 class CodeforcesContestsLoader: ContestsLoader(type = ContestsLoaders.codeforces) {
-    override suspend fun loadContests(platform: Contest.Platform) =
-        CodeforcesApi.getContests().map { contest ->
+    override suspend fun loadContests(
+        platform: Contest.Platform,
+        dateConstraints: ContestDateConstraints.Current
+    ) = CodeforcesApi.getContests()
+        .filter { dateConstraints.check(startTime = it.startTime, duration = it.duration) }
+        .map { contest ->
             Contest(
                 platform = Contest.Platform.codeforces,
                 id = contest.id.toString(),
