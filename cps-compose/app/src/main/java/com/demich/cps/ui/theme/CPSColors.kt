19567a818b8a5b51c4edb5385f3d7d8581140776
demidenko <devit15@yandex.ru>
2023-03-11 00:20:00 +0700

    cpsColors reworked

--- cps-compose/app/src/main/java/com/demich/cps/ui/theme/CPSColors.kt
+++ cps-compose/app/src/main/java/com/demich/cps/ui/theme/CPSColors.kt
@@ -1,93 +1,99 @@
 package com.demich.cps.ui.theme
 
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Colors
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.graphics.Color
 import com.demich.cps.accounts.managers.HandleColor
 
-object cpsColors {
-    //TODO: object with vals instead of getters
+internal val LocalCPSColors = compositionLocalOf<CPSColors> { throw IllegalAccessError() }
 
-    private val isLight: Boolean
-        @Composable
-        @ReadOnlyComposable
-        get() = MaterialTheme.colors.isLight
-
-    val accent: Color
-        @Composable
-        get() = MaterialTheme.colors.primary
-
-    val content: Color
-        @Composable
-        get() = MaterialTheme.colors.onBackground
-
-    val contentAdditional: Color
-        @Composable
-        get() = if (isLight) Color(118, 118, 118) else Color(147, 147, 147)
-
-    val background: Color
-        @Composable
-        get() = MaterialTheme.colors.background
-
-    val backgroundAdditional: Color
-        @Composable
-        get() = if (isLight) Color(234, 234, 234) else Color(36, 36, 36)
-
-    val backgroundNavigation: Color
-        @Composable
-        get() = if (isLight) Color(255, 255, 255) else Color(0, 0, 0)
-
-    val divider: Color
-        //@Composable
-        get() = Color(85, 85, 85)
-
-    val success: Color
-        @Composable
-        get() = if (isLight) Color(0, 128, 0) else Color(51, 153, 51)
-
-    val error: Color
-        @Composable
-        get() = MaterialTheme.colors.error
-
-    val votedRatingPositive: Color
-        @Composable
-        get() = success
-
-    val votedRatingNegative: Color
-        @Composable
-        get() = if (isLight) Color(128, 128, 128) else Color(150, 150, 150)
-
-    val newEntry: Color
-        @Composable
-        get() = if (isLight) Color(0xFF669900) else Color(0xFF99CC00)
-
+val cpsColors: CPSColors
     @Composable
-    fun handleColor(handleColor: HandleColor): Color {
-        if (isLight) {
-            return when(handleColor) {
-                HandleColor.GRAY -> Color(0xFF808080)
-                HandleColor.BROWN -> Color(0xFF804000)
-                HandleColor.GREEN -> Color(0xFF008000)
-                HandleColor.CYAN -> Color(0xFF03A89E)
-                HandleColor.BLUE -> Color(0xFF0000FF)
-                HandleColor.VIOLET -> Color(0xFFAA00AA)
-                HandleColor.YELLOW -> Color(0xFFDDC000)
-                HandleColor.ORANGE -> Color(0xFFFF8000)
-                HandleColor.RED -> Color(0xFFFF0000)
-            }
-        } else {
-            return when(handleColor) {
-                HandleColor.GRAY -> Color(0xFF888888)
-                HandleColor.BROWN -> Color(0xFF80461B)
-                HandleColor.GREEN -> Color(0xFF009000)
-                HandleColor.CYAN -> Color(0xFF00A89E)
-                HandleColor.BLUE -> Color(0xFF0F68F0)
-                HandleColor.VIOLET -> Color(0xFFB04ECC)
-                HandleColor.YELLOW -> Color(0xFFCCCC00)
-                HandleColor.ORANGE -> Color(0xFFFB8000)
-                HandleColor.RED -> Color(0xFFED301D)
-            }
-        }
+    @ReadOnlyComposable
+    get() = LocalCPSColors.current
+
+class CPSColors(
+    val accent: Color,
+    val content: Color,
+    val contentAdditional: Color,
+    val background: Color,
+    val backgroundAdditional: Color,
+    val backgroundNavigation: Color,
+    val divider: Color,
+    val success: Color,
+    val error: Color,
+    val votedRatingNegative: Color,
+    val newEntry: Color,
+    private val materialInitColors: () -> Colors,
+    handleColor: (HandleColor) -> Color
+) {
+    val votedRatingPositive: Color get() = success
+
+    private val handleColors = HandleColor.values().map(handleColor)
+    fun handleColor(handleColor: HandleColor): Color = handleColors[handleColor.ordinal]
+
+    fun materialColors() = materialInitColors().copy(
+        background = background,
+        primary = accent,
+        onBackground = content,
+        error = error
+    )
+}
+
+internal val CPSLightColors = CPSColors(
+    accent = Color(21, 101, 192),
+    content = Color(36, 36, 36),
+    contentAdditional = Color(118, 118, 118),
+    background = Color(248, 248, 248),
+    backgroundAdditional = Color(234, 234, 234),
+    backgroundNavigation = Color(255, 255, 255),
+    divider = Color(85, 85, 85),
+    success = Color(0, 128, 0),
+    error = Color(221, 34, 34),
+    votedRatingNegative = Color(128, 128, 128),
+    newEntry = Color(0xFF669900),
+    materialInitColors = ::lightColors
+) {
+    when (it) {
+        HandleColor.GRAY -> Color(0xFF808080)
+        HandleColor.BROWN -> Color(0xFF804000)
+        HandleColor.GREEN -> Color(0xFF008000)
+        HandleColor.CYAN -> Color(0xFF03A89E)
+        HandleColor.BLUE -> Color(0xFF0000FF)
+        HandleColor.VIOLET -> Color(0xFFAA00AA)
+        HandleColor.YELLOW -> Color(0xFFDDC000)
+        HandleColor.ORANGE -> Color(0xFFFF8000)
+        HandleColor.RED -> Color(0xFFFF0000)
+    }
+}
+
+internal val CPSDarkColors = CPSColors(
+    accent = Color(0, 153, 204),
+    content = Color(212, 212, 212),
+    contentAdditional = Color(147, 147, 147),
+    background = Color(18, 18, 18),
+    backgroundAdditional = Color(36, 36, 36),
+    backgroundNavigation = Color(0, 0, 0),
+    divider = Color(85, 85, 85),
+    success = Color(51, 153, 51),
+    error = Color(200, 64, 64),
+    votedRatingNegative = Color(150, 150, 150),
+    newEntry = Color(0xFF99CC00),
+    materialInitColors = ::darkColors
+) {
+    when (it) {
+        HandleColor.GRAY -> Color(0xFF888888)
+        HandleColor.BROWN -> Color(0xFF80461B)
+        HandleColor.GREEN -> Color(0xFF009000)
+        HandleColor.CYAN -> Color(0xFF00A89E)
+        HandleColor.BLUE -> Color(0xFF0F68F0)
+        HandleColor.VIOLET -> Color(0xFFB04ECC)
+        HandleColor.YELLOW -> Color(0xFFCCCC00)
+        HandleColor.ORANGE -> Color(0xFFFB8000)
+        HandleColor.RED -> Color(0xFFED301D)
     }
 }
--- cps-compose/app/src/main/java/com/demich/cps/ui/theme/Theme.kt
+++ cps-compose/app/src/main/java/com/demich/cps/ui/theme/Theme.kt
@@ -1,12 +1,13 @@
 package com.demich.cps.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.*
+import androidx.compose.material.LocalContentAlpha
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -14,20 +15,6 @@ import androidx.compose.ui.unit.sp
 import com.demich.cps.ui.settingsUI
 import com.demich.cps.utils.context
 import com.demich.cps.utils.rememberCollect
-
-private val LightColorPalette = lightColors(
-    background = Color(248, 248, 248),
-    primary = Color(21, 101, 192),
-    onBackground = Color(36, 36, 36),
-    error = Color(221, 34, 34)
-)
-
-private val DarkColorPalette = darkColors(
-    background = Color(18, 18, 18),
-    primary = Color(0, 153, 204),
-    onBackground = Color(212, 212, 212),
-    error = Color(200, 64, 64)
-)
 
 
 enum class DarkLightMode {
@@ -38,7 +25,6 @@ enum class DarkLightMode {
 private fun DarkLightMode.isDarkMode(): Boolean =
     if (this == DarkLightMode.SYSTEM) isSystemInDarkTheme() else this == DarkLightMode.DARK
 
-
 val LocalUseOriginalColors = compositionLocalOf { false }
 
 @Composable
@@ -46,8 +32,9 @@ fun CPSTheme(content: @Composable () -> Unit) {
     val context = context
     val darkLightMode by rememberCollect { context.settingsUI.darkLightMode.flow }
     val useOriginalColors by rememberCollect { context.settingsUI.useOriginalColors.flow }
+    val colors = if (darkLightMode.isDarkMode()) CPSDarkColors else CPSLightColors
     MaterialTheme(
-        colors = if (darkLightMode.isDarkMode()) DarkColorPalette else LightColorPalette,
+        colors = colors.materialColors(),
         typography = Typography(
             body1 = TextStyle(
                 fontFamily = FontFamily.Default,
@@ -58,6 +45,7 @@ fun CPSTheme(content: @Composable () -> Unit) {
         )
     ) {
         CompositionLocalProvider(
+            LocalCPSColors provides colors,
             LocalUseOriginalColors provides useOriginalColors,
             LocalContentAlpha provides 1f,
             content = content
