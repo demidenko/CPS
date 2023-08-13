c180ca1dd264aac94e085ba55fd6cf79873cedcc
demidenko <devit15@yandex.ru>
2023-08-13 23:47:42 +0700

    ItemizedMutablePreferences

--- cps-compose/datastore_itemized/src/main/java/com/demich/datastore_itemized/ItemizedPreferences.kt
+++ cps-compose/datastore_itemized/src/main/java/com/demich/datastore_itemized/ItemizedPreferences.kt
@@ -1,6 +1,8 @@
 package com.demich.datastore_itemized
 
+import androidx.datastore.preferences.core.MutablePreferences
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
@@ -9,5 +11,21 @@ class ItemizedPreferences(private val preferences: Preferences) {
         item.converter.getFrom(preferences)
 }
 
+class ItemizedMutablePreferences(private val preferences: MutablePreferences) {
+    operator fun<T> get(item: DataStoreItem<T>): T =
+        item.converter.getFrom(preferences)
+
+    operator fun<T> set(item: DataStoreItem<T>, value: T) =
+        item.converter.setTo(preferences, value)
+
+    fun<T> remove(item: DataStoreItem<T>) {
+        preferences.remove(item.converter.key)
+    }
+}
+
 fun<D: ItemizedDataStore, R> D.flowBy(transform: D.(ItemizedPreferences) -> R): Flow<R> =
     dataStore.data.map { transform(ItemizedPreferences(it)) }
+
+suspend fun<D: ItemizedDataStore> D.edit(block: D.(ItemizedMutablePreferences) -> Unit) {
+    dataStore.edit { block(ItemizedMutablePreferences(it)) }
+}
