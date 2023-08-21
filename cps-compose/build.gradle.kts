e8e654b71c12014933e443016c31bfa66a7888d1
demidenko <devit15@yandex.ru>
2023-08-21 22:14:43 +0700

    to kts

---
+++ cps-compose/build.gradle.kts
@@ -0,0 +1,14 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    val gradle_version = "8.1.0"
+    id("com.android.application") version gradle_version apply false
+    id("com.android.library") version gradle_version apply false
+    val kotlin_version = "1.9.0"
+    id("org.jetbrains.kotlin.android") version kotlin_version apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version kotlin_version apply false
+    id("com.google.devtools.ksp") version "$kotlin_version-+" apply false
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
