dbc0a2755be9e54e0abc6d1273e774db3a90ff5a
demidenko <devit15@yandex.ru>
2023-08-22 15:07:45 +0700

    moving

--- cps-compose/app/src/main/java/com/demich/cps/accounts/rating_graph/RatingGraph.kt
+++ cps-compose/app/src/main/java/com/demich/cps/accounts/rating_graph/RatingGraph.kt
@@ -1,6 +1,5 @@
 package com.demich.cps.accounts.rating_graph
 
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.gestures.detectTransformGestures
@@ -11,19 +10,9 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.clipToBounds
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.graphics.drawscope.Fill
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
-import com.demich.cps.accounts.HandleColor
 import com.demich.cps.accounts.managers.*
 import com.demich.cps.accounts.userinfo.RatedUserInfo
 import com.demich.cps.ui.CPSIconButton
@@ -37,7 +26,6 @@ import com.demich.cps.utils.jsonCPS
 import com.demich.cps.utils.saver
 import kotlinx.coroutines.launch
 import kotlinx.datetime.Instant
-import kotlin.math.round
 import kotlin.time.Duration.Companion.days
 
 
@@ -155,13 +143,11 @@ private fun RatingGraph(
 
     val translator = rememberCoordinateTranslator()
 
-    val currentTime = remember(ratingChanges) { getCurrentTime() }
-    var filterType by rememberSaveable(ratingChanges) {
-        if (ratingChanges.isNotEmpty()) {
-            translator.setWindow(
-                createBounds(ratingChanges = ratingChanges, filterType = RatingFilterType.ALL, now = currentTime)
-            )
-        }
+    val currentTime = remember { getCurrentTime() }
+    var filterType by rememberSaveable {
+        translator.setWindow(
+            createBounds(ratingChanges = ratingChanges, filterType = RatingFilterType.ALL, now = currentTime)
+        )
         mutableStateOf(RatingFilterType.ALL)
     }
 
@@ -240,18 +226,7 @@ private fun RatingGraphHeader(
         require(ratingChanges.isNotEmpty())
         TextButtonsSelectRow(
             values = remember(ratingChanges, currentTime) {
-                buildList {
-                    if (ratingChanges.size > 10) add(RatingFilterType.LAST_10)
-                    val firstInMonth = ratingChanges.indexOfFirst { it.date >= currentTime - 30.days }
-                    val firstInYear = ratingChanges.indexOfFirst { it.date >= currentTime - 365.days }
-                    if (firstInMonth != -1 && firstInMonth > 0) {
-                        add(RatingFilterType.LAST_MONTH)
-                    }
-                    if (firstInYear != -1 && firstInYear > 0 && firstInYear != firstInMonth) {
-                        add(RatingFilterType.LAST_YEAR)
-                    }
-                    if (isNotEmpty()) add(index = 0, RatingFilterType.ALL)
-                }
+                makeValidFilters(ratingChanges, currentTime)
             },
             selectedValue = selectedFilterType,
             text = RatingFilterType::title,
@@ -261,169 +236,17 @@ private fun RatingGraphHeader(
     }
 }
 
-@Composable
-private fun RatingGraphCanvas(
-    ratingChanges: List<RatingChange>,
-    manager: RatedAccountManager<out RatedUserInfo>,
-    rectangles: RatingGraphRectangles,
-    translator: CoordinateTranslator,
-    currentTime: Instant,
-    filterType: RatingFilterType,
-    selectedRatingChange: RatingChange?,
-    modifier: Modifier = Modifier
-) {
-    val managerSupportedColors = remember(manager.type) {
-        HandleColor.entries.filter {
-            runCatching { manager.originalColor(it) }.isSuccess
+private fun makeValidFilters(ratingChanges: List<RatingChange>, currentTime: Instant) =
+    buildList {
+        if (ratingChanges.size > 10) add(RatingFilterType.LAST_10)
+        val firstInMonth = ratingChanges.indexOfFirst { it.date >= currentTime - 30.days }
+        val firstInYear = ratingChanges.indexOfFirst { it.date >= currentTime - 365.days }
+        if (firstInMonth != -1 && firstInMonth > 0) {
+            add(RatingFilterType.LAST_MONTH)
         }
+        if (firstInYear != -1 && firstInYear > 0 && firstInYear != firstInMonth) {
+            add(RatingFilterType.LAST_YEAR)
+        }
+        if (isNotEmpty()) add(index = 0, RatingFilterType.ALL)
     }
 
-    val timeMarkers: List<Instant> = remember(key1 = filterType, key2 = ratingChanges) {
-        if (filterType == RatingFilterType.ALL || ratingChanges.size < 2) emptyList()
-        else {
-            createBounds(ratingChanges, filterType, currentTime).run {
-                listOf(startTime, endTime)
-            }
-        }
-    }
-
-    RatingGraphCanvas(
-        ratingPoints = ratingChanges.map { it.toPoint() }.sortedBy { it.x },
-        translator = translator,
-        selectedPoint = selectedRatingChange?.toPoint(),
-        markVerticals = timeMarkers.map { it.epochSeconds },
-        colorsMap = managerSupportedColors.associateWith { manager.colorFor(handleColor = it) },
-        rectangles = rectangles,
-        modifier = modifier
-    )
-}
-
-@Composable
-private fun RatingGraphCanvas(
-    ratingPoints: List<Point>,
-    translator: CoordinateTranslator,
-    selectedPoint: Point?,
-    markVerticals: List<Long>,
-    colorsMap: Map<HandleColor, Color>,
-    rectangles: RatingGraphRectangles,
-    modifier: Modifier = Modifier,
-    circleRadius: Float = 6f,
-    circleBorderWidth: Float = 3f,
-    pathWidth: Float = 4f,
-    shadowOffset: Offset = Offset(4f, 4f),
-    shadowAlpha: Float = 0.3f,
-    selectedPointRadiusMultiplier: Float = 1.5f
-) {
-    fun radiusMultiplier(point: Point) = if (selectedPoint == point) selectedPointRadiusMultiplier else 1f
-
-    val dashEffect = remember { PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f) }
-
-    Canvas(
-        modifier = modifier
-            .fillMaxSize()
-            .clipToBounds()
-    ) {
-        translator.canvasSize = size
-        translator.borderX = circleRadius * 5
-
-        val ratingPath = Path().apply {
-            ratingPoints.forEachIndexed { index, point ->
-                val (px, py) = translator.pointToOffset(point)
-                if (index == 0) moveTo(px, py)
-                else lineTo(px, py)
-            }
-        }
-
-        //rating filled areas
-        rectangles.forEach { point, handleColor ->
-            val (px, py) = translator.pointToOffset(point).let {
-                round(it.x) to round(it.y)
-            }
-            drawRect(
-                color = colorsMap.getValue(handleColor),
-                topLeft = Offset.Zero,
-                size = Size(px, py)
-            )
-        }
-
-        //time dashes
-        if (selectedPoint != null) {
-            val p = translator.pointToOffset(selectedPoint)
-            drawLine(
-                color = Color.Black,
-                start = Offset(p.x, 0f),
-                end = p,
-                pathEffect = dashEffect
-            )
-        } else {
-            markVerticals.forEach { x ->
-                val p = translator.pointToOffset(x, 0)
-                drawLine(
-                    color = Color.Black,
-                    start = Offset(p.x, 0f),
-                    end = Offset(p.x, size.height),
-                    pathEffect = dashEffect
-                )
-            }
-        }
-
-        //shadow
-        translate(left = shadowOffset.x, top = shadowOffset.y) {
-            //shadow of rating path
-            drawPath(
-                path = ratingPath,
-                color = Color.Black,
-                style = Stroke(width = pathWidth),
-                alpha = shadowAlpha
-            )
-            //shadow of rating points
-            ratingPoints.forEach { point ->
-                val center = translator.pointToOffset(point)
-                drawCircle(
-                    color = Color.Black,
-                    radius = (circleRadius + circleBorderWidth) * radiusMultiplier(point),
-                    center = center,
-                    style = Fill,
-                    alpha = shadowAlpha
-                )
-            }
-        }
-
-        //rating path
-        drawPath(
-            path = ratingPath,
-            color = Color.Black,
-            style = Stroke(width = pathWidth)
-        )
-
-        //rating points
-        rectangles.iterateWithHandleColor(ratingPoints) { point, handleColor ->
-            val center = translator.pointToOffset(point)
-            drawCircle(
-                color = Color.Black,
-                radius = (circleRadius + circleBorderWidth) * radiusMultiplier(point),
-                center = center,
-                style = Fill
-            )
-            drawCircle(
-                color = colorsMap.getValue(handleColor),
-                radius = circleRadius * radiusMultiplier(point),
-                center = center,
-                style = Fill
-            )
-            if (point == selectedPoint) {
-                drawCircle(
-                    color = Color.Black,
-                    radius = (circleRadius / 2) * radiusMultiplier(point),
-                    center = center,
-                    style = Fill
-                )
-            }
-        }
-    }
-}
-
-
-internal data class Point(val x: Long, val y: Long)
-
-internal fun RatingChange.toPoint() = Point(x = date.epochSeconds, y = rating.toLong())
---
+++ cps-compose/app/src/main/java/com/demich/cps/accounts/rating_graph/RatingGraphCanvas.kt
@@ -0,0 +1,189 @@
+package com.demich.cps.accounts.rating_graph
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.drawscope.Fill
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.translate
+import com.demich.cps.accounts.HandleColor
+import com.demich.cps.accounts.managers.RatedAccountManager
+import com.demich.cps.accounts.managers.RatingChange
+import com.demich.cps.accounts.managers.colorFor
+import com.demich.cps.accounts.userinfo.RatedUserInfo
+import kotlinx.datetime.Instant
+import kotlin.math.round
+
+@Composable
+internal fun RatingGraphCanvas(
+    ratingChanges: List<RatingChange>,
+    manager: RatedAccountManager<out RatedUserInfo>,
+    rectangles: RatingGraphRectangles,
+    translator: CoordinateTranslator,
+    currentTime: Instant,
+    filterType: RatingFilterType,
+    selectedRatingChange: RatingChange?,
+    modifier: Modifier = Modifier
+) {
+    val managerSupportedColors = remember(manager.type) {
+        HandleColor.entries.filter {
+            runCatching { manager.originalColor(it) }.isSuccess
+        }
+    }
+
+    val timeMarkers: List<Instant> = remember(key1 = filterType, key2 = ratingChanges) {
+        if (filterType == RatingFilterType.ALL || ratingChanges.size < 2) emptyList()
+        else {
+            createBounds(ratingChanges, filterType, currentTime).run {
+                listOf(startTime, endTime)
+            }
+        }
+    }
+
+    RatingGraphCanvas(
+        ratingPoints = ratingChanges.map { it.toPoint() }.sortedBy { it.x },
+        translator = translator,
+        selectedPoint = selectedRatingChange?.toPoint(),
+        markVerticals = timeMarkers.map { it.epochSeconds },
+        colorsMap = managerSupportedColors.associateWith { manager.colorFor(handleColor = it) },
+        rectangles = rectangles,
+        modifier = modifier
+    )
+}
+
+@Composable
+private fun RatingGraphCanvas(
+    ratingPoints: List<Point>,
+    translator: CoordinateTranslator,
+    selectedPoint: Point?,
+    markVerticals: List<Long>,
+    colorsMap: Map<HandleColor, Color>,
+    rectangles: RatingGraphRectangles,
+    modifier: Modifier = Modifier,
+    circleRadius: Float = 6f,
+    circleBorderWidth: Float = 3f,
+    pathWidth: Float = 4f,
+    shadowOffset: Offset = Offset(4f, 4f),
+    shadowAlpha: Float = 0.3f,
+    selectedPointRadiusMultiplier: Float = 1.5f
+) {
+    fun radiusMultiplier(point: Point) = if (selectedPoint == point) selectedPointRadiusMultiplier else 1f
+
+    val dashEffect = remember { PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f) }
+
+    Canvas(
+        modifier = modifier
+            .fillMaxSize()
+            .clipToBounds()
+    ) {
+        translator.canvasSize = size
+        translator.borderX = circleRadius * 5
+
+        val ratingPath = Path().apply {
+            ratingPoints.forEachIndexed { index, point ->
+                val (px, py) = translator.pointToOffset(point)
+                if (index == 0) moveTo(px, py)
+                else lineTo(px, py)
+            }
+        }
+
+        //rating filled areas
+        rectangles.forEach { point, handleColor ->
+            val (px, py) = translator.pointToOffset(point).let {
+                round(it.x) to round(it.y)
+            }
+            drawRect(
+                color = colorsMap.getValue(handleColor),
+                topLeft = Offset.Zero,
+                size = Size(px, py)
+            )
+        }
+
+        //time dashes
+        if (selectedPoint != null) {
+            val p = translator.pointToOffset(selectedPoint)
+            drawLine(
+                color = Color.Black,
+                start = Offset(p.x, 0f),
+                end = p,
+                pathEffect = dashEffect
+            )
+        } else {
+            markVerticals.forEach { x ->
+                val p = translator.pointToOffset(x, 0)
+                drawLine(
+                    color = Color.Black,
+                    start = Offset(p.x, 0f),
+                    end = Offset(p.x, size.height),
+                    pathEffect = dashEffect
+                )
+            }
+        }
+
+        //shadow
+        translate(left = shadowOffset.x, top = shadowOffset.y) {
+            //shadow of rating path
+            drawPath(
+                path = ratingPath,
+                color = Color.Black,
+                style = Stroke(width = pathWidth),
+                alpha = shadowAlpha
+            )
+            //shadow of rating points
+            ratingPoints.forEach { point ->
+                val center = translator.pointToOffset(point)
+                drawCircle(
+                    color = Color.Black,
+                    radius = (circleRadius + circleBorderWidth) * radiusMultiplier(point),
+                    center = center,
+                    style = Fill,
+                    alpha = shadowAlpha
+                )
+            }
+        }
+
+        //rating path
+        drawPath(
+            path = ratingPath,
+            color = Color.Black,
+            style = Stroke(width = pathWidth)
+        )
+
+        //rating points
+        rectangles.iterateWithHandleColor(ratingPoints) { point, handleColor ->
+            val center = translator.pointToOffset(point)
+            drawCircle(
+                color = Color.Black,
+                radius = (circleRadius + circleBorderWidth) * radiusMultiplier(point),
+                center = center,
+                style = Fill
+            )
+            drawCircle(
+                color = colorsMap.getValue(handleColor),
+                radius = circleRadius * radiusMultiplier(point),
+                center = center,
+                style = Fill
+            )
+            if (point == selectedPoint) {
+                drawCircle(
+                    color = Color.Black,
+                    radius = (circleRadius / 2) * radiusMultiplier(point),
+                    center = center,
+                    style = Fill
+                )
+            }
+        }
+    }
+}
+
+internal data class Point(val x: Long, val y: Long)
+
+internal fun RatingChange.toPoint() = Point(x = date.epochSeconds, y = rating.toLong())
