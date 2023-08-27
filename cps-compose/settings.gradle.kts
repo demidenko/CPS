1d7187808ff64af68436b72c0713658082524ebb
demidenko <devit15@yandex.ru>
2023-08-27 18:19:48 +0700

    to kts

--- cps-compose/settings.gradle.kts
+++ cps-compose/settings.gradle.kts
@@ -13,7 +13,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         //https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog-declaration
-        libs {
+        create("libs") {
             library("kotlin-serialization", "org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
             library("kotlin-datetime", "org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
             library("kotlin-coroutines", "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
@@ -24,15 +24,16 @@ dependencyResolutionManagement {
         }
     }
 }
+
 rootProject.name = "CPS"
-include ':app'
-include ':datastore_itemized'
-include ':data:platforms:api'
-include ':data:platforms:utils'
-include ':data:accounts:userinfo'
-include ':data:contests:database'
-include ':data:contests:loading'
-include ':features:room_base'
-include ':features:codeforces_lost:database'
-include ':features:codeforces_follow:database'
-include ':features:contests_loading_engine'
+include(":app")
+include(":datastore_itemized")
+include(":data:platforms:api")
+include(":data:platforms:utils")
+include(":data:accounts:userinfo")
+include(":data:contests:database")
+include(":data:contests:loading")
+include(":features:room_base")
+include(":features:codeforces_lost:database")
+include(":features:codeforces_follow:database")
+include(":features:contests_loading_engine")
