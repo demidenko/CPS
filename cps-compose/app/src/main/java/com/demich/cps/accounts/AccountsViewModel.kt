d76d22dc2cdfd31c069449447109ebf362f854dc
demidenko <devit15@yandex.ru>
2023-08-28 18:49:01 +0700

    map of flow to flow of map

--- cps-compose/app/src/main/java/com/demich/cps/accounts/AccountsViewModel.kt
+++ cps-compose/app/src/main/java/com/demich/cps/accounts/AccountsViewModel.kt
@@ -16,12 +16,13 @@ import com.demich.cps.ui.bottomprogressbar.ProgressBarInfo
 import com.demich.cps.ui.bottomprogressbar.ProgressBarsViewModel
 import com.demich.cps.utils.LoadingStatus
 import com.demich.cps.utils.backgroundDataLoader
+import com.demich.cps.utils.edit
 import com.demich.cps.utils.sharedViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.takeWhile
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
 
@@ -29,30 +30,30 @@ import kotlinx.coroutines.launch
 fun accountsViewModel(): AccountsViewModel = sharedViewModel()
 
 class AccountsViewModel: ViewModel() {
-    private val loadingStatuses = mutableMapOf<AccountManagerType, MutableStateFlow<LoadingStatus>>()
-
-    private fun mutableLoadingStatusFor(manager: AccountManager<out UserInfo>): MutableStateFlow<LoadingStatus> =
-        loadingStatuses.getOrPut(manager.type) { MutableStateFlow(LoadingStatus.PENDING) }
+    private val loadingStatuses = MutableStateFlow(emptyMap<AccountManagerType, LoadingStatus>())
 
     fun flowOfLoadingStatus(manager: AccountManager<out UserInfo>): Flow<LoadingStatus> =
-        mutableLoadingStatusFor(manager)
+        loadingStatuses.map { it[manager.type] ?: LoadingStatus.PENDING }
 
+    private fun setLoadingStatus(manager: AccountManager<out UserInfo>, loadingStatus: LoadingStatus) =
+        loadingStatuses.edit {
+            if (loadingStatus == LoadingStatus.PENDING) remove(manager.type)
+            else this[manager.type] = loadingStatus
+        }
 
     fun<U: UserInfo> reload(manager: AccountManager<U>, context: Context) {
+        if (loadingStatuses.value[manager.type] == LoadingStatus.LOADING) return
         viewModelScope.launch {
             val dataStore = manager.dataStore(context)
             val savedInfo = dataStore.getSavedInfo() ?: return@launch
 
-            val loadingStatusState = mutableLoadingStatusFor(manager)
-            if (loadingStatusState.value == LoadingStatus.LOADING) return@launch
-
-            loadingStatusState.value = LoadingStatus.LOADING
+            setLoadingStatus(manager, LoadingStatus.LOADING)
             val info = manager.loadInfo(savedInfo.userId)
 
             if (info.status == STATUS.FAILED) {
-                loadingStatusState.value = LoadingStatus.FAILED
+                setLoadingStatus(manager, LoadingStatus.FAILED)
             } else {
-                loadingStatusState.value = LoadingStatus.PENDING
+                setLoadingStatus(manager, LoadingStatus.PENDING)
                 dataStore.setSavedInfo(info)
             }
         }
@@ -60,10 +61,7 @@ class AccountsViewModel: ViewModel() {
 
     fun<U: UserInfo> delete(manager: AccountManager<U>, context: Context) {
         viewModelScope.launch {
-            mutableLoadingStatusFor(manager).update {
-                require(it != LoadingStatus.LOADING)
-                LoadingStatus.PENDING
-            }
+            setLoadingStatus(manager, LoadingStatus.PENDING)
             manager.dataStore(context).deleteSavedInfo()
         }
     }
@@ -80,17 +78,16 @@ class AccountsViewModel: ViewModel() {
             progress.value = ProgressBarInfo(title = "clist import", total = supported.size)
             supported.map { (type, userId) ->
                 val manager = allAccountManagers.first { it.type == type }
-                val loadingStatusState = mutableLoadingStatusFor(manager)
                 launch {
                     //wait for loading stops
-                    loadingStatusState.takeWhile { it == LoadingStatus.LOADING }.collect()
+                    loadingStatuses.takeWhile { it[type] == LoadingStatus.LOADING }.collect()
                     if (userId.equals(manager.dataStore(context).getSavedInfo()?.userId, ignoreCase = true)) {
                         //if userId is same just reload to prevent replace by FAILED
                         reload(manager, context)
                     } else {
-                        loadingStatusState.value = LoadingStatus.LOADING
+                        setLoadingStatus(manager, LoadingStatus.LOADING)
                         loadAndSave(manager, userId, context)
-                        loadingStatusState.value = LoadingStatus.PENDING
+                        setLoadingStatus(manager, LoadingStatus.PENDING)
                     }
                     progress.value++
                 }
