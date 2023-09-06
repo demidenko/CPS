56d2e601a278cdc2ece2cf3b42c1590b42359b77
demidenko <devit15@yandex.ru>
2023-09-07 06:54:30 +0700

    code ref

--- cps-compose/app/src/main/java/com/demich/cps/contests/settings/ContestsSettingsDataStore.kt
+++ cps-compose/app/src/main/java/com/demich/cps/contests/settings/ContestsSettingsDataStore.kt
@@ -9,7 +9,6 @@ import com.demich.cps.platforms.api.ClistResource
 import com.demich.cps.utils.jsonCPS
 import com.demich.datastore_itemized.ItemizedDataStore
 import com.demich.datastore_itemized.dataStoreWrapper
-import kotlinx.datetime.Instant
 import kotlin.time.Duration.Companion.days
 
 val Context.settingsContests: ContestsSettingsDataStore
@@ -29,27 +28,24 @@ class ContestsSettingsDataStore(context: Context): ItemizedDataStore(context.con
     val clistApiAccess = jsonCPS.item(name = "clist_api_access", defaultValue = ClistApi.ApiAccess("", ""))
     val clistAdditionalResources = jsonCPS.itemList<ClistResource>(name = "clist_additional_resources")
 
-    val contestsDateConstraints = jsonCPS.item(
-        name = "contests_date_constraints",
-        defaultValue = {
-            ContestDateBaseConstraints(
-                maxDuration = 30.days,
-                nowToStartTimeMaxDuration = 120.days,
-                endTimeToNowMaxDuration = 7.days
-            )
-        }
-    )
+    val contestsDateConstraints = jsonCPS.item(name = "contests_date_constraints") {
+        ContestDateBaseConstraints(
+            maxDuration = 30.days,
+            nowToStartTimeMaxDuration = 120.days,
+            endTimeToNowMaxDuration = 7.days
+        )
+    }
 
-    val contestsLoadersPriorityLists = jsonCPS.itemMap(name = "loading_priorities", defaultValue = ::makeDefaultLoadingPriorities)
+    val contestsLoadersPriorityLists = jsonCPS.itemMap(name = "loading_priorities") {
+        ContestsLoaderType.entries.sortedWith(
+            compareByDescending<ContestsLoaderType> { it.supportedPlatforms.size }.thenBy { it.ordinal }
+        ).let { loaders ->
+            Contest.platforms.associateWith { platform ->
+                loaders.filter { platform in it.supportedPlatforms }
+            }
+        }
+    }
 
     val enabledAutoUpdate = itemBoolean(name = "auto_update", defaultValue = true)
 }
 
-private fun makeDefaultLoadingPriorities() =
-    ContestsLoaderType.entries.sortedWith(
-        compareByDescending<ContestsLoaderType> { it.supportedPlatforms.size }.thenBy { it.ordinal }
-    ).let { loaders ->
-        Contest.platforms.associateWith { platform ->
-            loaders.filter { platform in it.supportedPlatforms }
-        }
-    }
