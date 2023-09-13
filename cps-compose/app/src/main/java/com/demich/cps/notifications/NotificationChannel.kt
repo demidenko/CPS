ba5cef9255a9421615e0b3cc6d22af224da1f1a0
demidenko <devit15@yandex.ru>
2023-09-13 17:16:43 +0700

    check before notify

--- cps-compose/app/src/main/java/com/demich/cps/notifications/NotificationChannel.kt
+++ cps-compose/app/src/main/java/com/demich/cps/notifications/NotificationChannel.kt
@@ -1,22 +1,33 @@
 package com.demich.cps.notifications
 
+import android.Manifest
 import android.app.Notification
 import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 
 class NotificationBuilder internal constructor(
     private val builder: NotificationCompat.Builder,
     val notificationId: Int,
-    private val notificationManager: NotificationManagerCompat
+    private val notificationManager: NotificationManagerCompat,
+    private val context: Context
 ) {
     fun edit(buildBody: NotificationCompat.Builder.() -> Unit) = builder.buildBody()
 
     fun build(): Notification = builder.build()
 
     @JvmName("notifyCustom")
-    //TODO: android 14 permissions
-    fun notify() = notificationManager.notify(notificationId, build())
+    fun notify() {
+        if (!notificationManager.areNotificationsEnabled()) return
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED)
+                return
+        }
+        notificationManager.notify(notificationId, build())
+    }
 }
 
 class NotificationChannelSingleId internal constructor(
@@ -30,7 +41,8 @@ class NotificationChannelSingleId internal constructor(
         return NotificationBuilder(
             builder = NotificationCompat.Builder(context, channelInfo.id).apply(buildBody),
             notificationId = notificationId,
-            notificationManager = notificationManager
+            notificationManager = notificationManager,
+            context = context
         )
     }
 
