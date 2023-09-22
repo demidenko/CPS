3f29b1ce00f5958282cdf9cbef543b0d255e753e
demidenko <devit15@yandex.ru>
2023-09-22 22:00:02 +0700

    extract calc

--- cps-compose/app/src/main/java/com/demich/cps/ui/lazylist/LazyColumnScrollBar.kt
+++ cps-compose/app/src/main/java/com/demich/cps/ui/lazylist/LazyColumnScrollBar.kt
@@ -1,5 +1,6 @@
 package com.demich.cps.ui.lazylist
 
+import androidx.compose.foundation.lazy.LazyListLayoutInfo
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithContent
@@ -19,26 +20,33 @@ fun Modifier.lazyColumnScrollBar(
     minimumScrollBarHeight: Dp = 10.dp
 ): Modifier = this.drawWithContent {
     drawContent()
-    val info = state.layoutInfo
-    val countOfVisible = info.visibleItemsInfo.size
+    state.layoutInfo.calculateSizes { windowSize, totalSize, beforeSize ->
+        val w = scrollBarWidth.toPx()
+        val barHeight = windowSize / totalSize * windowSize
+        val h = max(barHeight, minimumScrollBarHeight.toPx())
+        val y = if (barHeight == h) beforeSize / totalSize * windowSize
+        else beforeSize / (totalSize - windowSize) * (windowSize - h)
+        drawRoundRect(
+            color = scrollBarColor,
+            topLeft = Offset(x = size.width - w, y = y),
+            size = Size(width = w, height = h),
+            cornerRadius = CornerRadius(w / 2)
+        )
+    }
+}
+
+private inline fun LazyListLayoutInfo.calculateSizes(
+    block: (windowSize: Int, totalSize: Float, beforeSize: Float) -> Unit
+) {
+    val countOfVisible = visibleItemsInfo.size
     if (countOfVisible > 0) {
-        val windowSize = info.viewportEndOffset - info.viewportStartOffset
-        val visibleItemsSize = info.visibleItemsInfo.sumOf { it.size }
+        val windowSize = viewportEndOffset - viewportStartOffset
+        val visibleItemsSize = visibleItemsInfo.sumOf { it.size }
         if (windowSize < visibleItemsSize) {
-            val itemSize: Float = visibleItemsSize.toFloat() / countOfVisible
-            val totalSize: Float = info.totalItemsCount * itemSize
-            val beforeSize: Float = state.firstVisibleItemIndex * itemSize - info.visibleItemsInfo.first().offset
-            val w = scrollBarWidth.toPx()
-            val barHeight = windowSize / totalSize * windowSize
-            val h = max(barHeight, minimumScrollBarHeight.toPx())
-            val y = if (barHeight == h) beforeSize / totalSize * windowSize
-                else beforeSize / (totalSize - windowSize) * (windowSize - h)
-            drawRoundRect(
-                color = scrollBarColor,
-                topLeft = Offset(x = size.width - w, y = y),
-                size = Size(width = w, height = h),
-                cornerRadius = CornerRadius(w / 2)
-            )
+            val itemAvgSize: Float = visibleItemsSize.toFloat() / countOfVisible
+            val totalSize: Float = totalItemsCount * itemAvgSize
+            val beforeSize: Float = visibleItemsInfo.first().run { index * itemAvgSize - offset }
+            block(windowSize, totalSize, beforeSize)
         }
     }
 }
