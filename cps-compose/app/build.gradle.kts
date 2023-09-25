b85d4557f2bb64a8f8babb4754afeecc93889a9e
demidenko <devit15@yandex.ru>
2023-09-25 21:31:25 +0700

    build

--- cps-compose/app/build.gradle.kts
+++ cps-compose/app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 26
         targetSdk = 34
 
-        versionCode = 379
+        versionCode = 380
         versionName = "1.9.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
