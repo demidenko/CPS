b4d08107f58e1e5f16b0b1b91fa9eeaf5bcbf9e1
demidenko <devit15@yandex.ru>
2023-11-10 00:42:59 +0700

    gradle upd

--- cps-compose/build.gradle.kts
+++ cps-compose/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    val gradle_version = "8.1.1"
+    val gradle_version = "8.1.3"
     id("com.android.application") version gradle_version apply false
     id("com.android.library") version gradle_version apply false
 
