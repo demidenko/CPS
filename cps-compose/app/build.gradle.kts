bbef5b56e8393833020b2a07417a8f3e4fc6c4e1
demidenko <devit15@yandex.ru>
2024-07-10 16:05:58 +0700

    kotlin 2

--- cps-compose/app/build.gradle.kts
+++ cps-compose/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
@@ -13,7 +14,7 @@ android {
         minSdk = 26
         targetSdk = 34
 
-        versionCode = 394
+        versionCode = 395
         versionName = "1.9.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -44,9 +45,6 @@ android {
     buildFeatures {
         compose = true
         buildConfig = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.14"
     }
 
     packaging {
--- cps-compose/build.gradle.kts
+++ cps-compose/build.gradle.kts
@@ -9,6 +9,8 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.ksp) apply false
+
+    alias(libs.plugins.compose.compiler) apply false
 }
 
 tasks.register<Delete>("clean") {
