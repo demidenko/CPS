ea5f6f2f7b6655c551c90e669d5837df417bb346
demidenko <devit15@yandex.ru>
2024-08-11 00:39:19 +0700

    fix (?) codechef parse rating

--- cps-compose/data/platforms/utils/src/main/java/com/demich/cps/platforms/utils/CodeChefUtils.kt
+++ cps-compose/data/platforms/utils/src/main/java/com/demich/cps/platforms/utils/CodeChefUtils.kt
@@ -7,10 +7,13 @@ import org.jsoup.Jsoup
 object CodeChefUtils {
     fun extractUserInfo(source: String, handle: String): CodeChefUserInfo =
         Jsoup.parse(source).run {
-            val rating = expectFirst("div.rating-ranks")
+            val rating = selectFirst("div.widget-rating")
+                ?.selectFirst("div.rating-header > div.rating-number")?.text()?.toInt()
+            /*val rating = expectFirst("div.rating-ranks")
                 .select("a")
                 .takeIf { it.any { it.text() != "Inactive" } }
                 ?.let { selectFirst("div.rating-header > div.rating-number")?.text()?.toInt() }
+             */
             val userName = expectFirst("section.user-details")
                 .selectFirst("span.m-username--link")
                 ?.text() ?: handle
