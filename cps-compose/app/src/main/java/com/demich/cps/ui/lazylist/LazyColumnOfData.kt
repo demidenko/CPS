1308e7e6d9fbc938323dcea6f836ce138f3e3acf
demidenko <devit15@yandex.ru>
2024-08-26 09:10:45 +0700

    init ScrollButton

--- cps-compose/app/src/main/java/com/demich/cps/ui/lazylist/LazyColumnOfData.kt
+++ cps-compose/app/src/main/java/com/demich/cps/ui/lazylist/LazyColumnOfData.kt
@@ -1,9 +1,12 @@
 package com.demich.cps.ui.lazylist
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyItemScope
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
@@ -11,19 +14,34 @@ fun<T> LazyColumnOfData(
     modifier: Modifier = Modifier,
     state: LazyListState = rememberLazyListState(),
     scrollBarEnabled: Boolean = true,
+    scrollToStartButtonEnabled: Boolean = false,
     items: () -> List<T>,
     key: ((item: T) -> Any)? = null,
     itemContent: @Composable LazyItemScope.(item: T) -> Unit
 ) {
-    LazyColumnWithScrollBar(
-        modifier = modifier,
-        state = state,
-        scrollBarEnabled = scrollBarEnabled
-    ) {
-        itemsNotEmpty(
-            items = items(),
-            key = key,
-            itemContent = itemContent
-        )
+    Box(modifier = modifier) {
+        LazyColumnWithScrollBar(
+            modifier = Modifier.fillMaxSize(),
+            state = state,
+            scrollBarEnabled = scrollBarEnabled
+        ) {
+            itemsNotEmpty(
+                items = items(),
+                key = key,
+                itemContent = itemContent
+            )
+        }
+
+        if (scrollToStartButtonEnabled) {
+            ScrollButton(listState = state, modifier = Modifier.align(Alignment.BottomEnd))
+        }
     }
+}
+
+@Composable
+private fun ScrollButton(
+    modifier: Modifier = Modifier,
+    listState: LazyListState
+) {
+    //TODO
 }
