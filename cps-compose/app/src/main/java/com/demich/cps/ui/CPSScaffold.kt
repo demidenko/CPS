32eb9a0d8ec12d6c828fe87ff11f5dce612447ab
demidenko <devit15@yandex.ru>
2024-08-30 22:12:57 +0700

    navbar insets on disabled

--- cps-compose/app/src/main/java/com/demich/cps/ui/CPSScaffold.kt
+++ cps-compose/app/src/main/java/com/demich/cps/ui/CPSScaffold.kt
@@ -10,11 +10,14 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
@@ -79,7 +82,10 @@ private fun Scaffold(
         animationSpec = switchAnimationSpec()
     )
 
-    Column(modifier = modifier) {
+    Column(modifier = modifier.let {
+        if (navigator.isBottomBarEnabled) it
+        else it.windowInsetsPadding(WindowInsets.navigationBars)
+    }) {
         Box(
             contentAlignment = Alignment.BottomCenter,
             modifier = Modifier
