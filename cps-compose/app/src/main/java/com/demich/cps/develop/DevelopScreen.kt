2603366bf240993147d8a90b3ea03fb5f7c90c44
demidenko <devit15@yandex.ru>
2024-09-02 05:02:36 +0700

    cf new entries size

--- cps-compose/app/src/main/java/com/demich/cps/develop/DevelopScreen.kt
+++ cps-compose/app/src/main/java/com/demich/cps/develop/DevelopScreen.kt
@@ -3,6 +3,7 @@ package com.demich.cps.develop
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -14,10 +15,14 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.demich.cps.accounts.managers.AccountManagerType
 import com.demich.cps.accounts.managers.RatedAccountManager
 import com.demich.cps.accounts.managers.allRatedAccountManagers
 import com.demich.cps.accounts.userinfo.RatedUserInfo
+import com.demich.cps.community.codeforces.CodeforcesNewEntriesDataStore
+import com.demich.cps.ui.CPSDefaults
 import com.demich.cps.ui.CPSIconButton
 import com.demich.cps.ui.CPSIcons
 import com.demich.cps.ui.CPSRadioButtonTitled
@@ -25,7 +30,10 @@ import com.demich.cps.ui.bottombar.AdditionalBottomBarBuilder
 import com.demich.cps.ui.bottomprogressbar.progressBarsViewModel
 import com.demich.cps.ui.lazylist.LazyColumnWithScrollBar
 import com.demich.cps.ui.theme.cpsColors
+import com.demich.cps.utils.collectAsState
+import com.demich.cps.utils.context
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import kotlin.random.Random
 import kotlin.time.Duration.Companion.seconds
@@ -34,7 +42,8 @@ import kotlin.time.Duration.Companion.seconds
 fun DevelopScreen() {
     //TestHandles(modifier = Modifier.fillMaxWidth())
     Column {
-        WorkersList(modifier = Modifier.fillMaxWidth())
+        WorkersList(modifier = Modifier.fillMaxWidth().weight(1f))
+        NewEntriesCfInfo(modifier = Modifier.fillMaxWidth().padding(all = 12.dp))
     }
 }
 
@@ -54,6 +63,21 @@ fun developAdditionalBottomBarBuilder(): AdditionalBottomBarBuilder = {
             }
         }
     }
+}
+
+@Composable
+private fun NewEntriesCfInfo(
+    modifier: Modifier = Modifier
+) {
+    val context = context
+    val count by collectAsState {
+        CodeforcesNewEntriesDataStore(context).commonNewEntries.flow.map { it.size }
+    }
+    Text(
+        modifier = modifier,
+        text = "cf new entries: $count",
+        style = CPSDefaults.MonospaceTextStyle.copy(fontSize = 16.sp)
+    )
 }
 
 
