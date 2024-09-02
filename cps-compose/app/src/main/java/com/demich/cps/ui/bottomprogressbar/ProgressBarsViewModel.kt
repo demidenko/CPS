f9271e6b0eacd3cea29c4bec9bac02f36098db05
demidenko <devit15@yandex.ru>
2024-09-02 21:17:47 +0700

    clean up

--- cps-compose/app/src/main/java/com/demich/cps/ui/bottomprogressbar/ProgressBarsViewModel.kt
+++ cps-compose/app/src/main/java/com/demich/cps/ui/bottomprogressbar/ProgressBarsViewModel.kt
@@ -10,6 +10,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import kotlin.reflect.KProperty
@@ -32,26 +33,26 @@ fun progressBarsViewModel(): ProgressBarsViewModel = sharedViewModel()
 
 class ProgressBarsViewModel: ViewModel() {
     private val progressesStateFlow = MutableStateFlow(emptyMap<String, ProgressBarInfo>())
-    fun flowOfProgresses(): Flow<Map<String, ProgressBarInfo>> = progressesStateFlow
+    fun flowOfProgresses(): StateFlow<Map<String, ProgressBarInfo>> = progressesStateFlow
 
     fun doJob(
         id: String,
         coroutineScope: CoroutineScope = viewModelScope,
-        block: suspend CoroutineScope.((ProgressBarInfo?) -> Unit) -> Unit
+        block: suspend CoroutineScope.((ProgressBarInfo) -> Unit) -> Unit
     ) {
-        var progressBarInfo: ProgressBarInfo? by object {
-            operator fun getValue(thisObj: Any?, property: KProperty<*>): ProgressBarInfo? =
-                progressesStateFlow.value[id]
+        coroutineScope.launch {
+            var progressBarInfo: ProgressBarInfo? by object {
+                operator fun getValue(thisObj: Any?, property: KProperty<*>): ProgressBarInfo? =
+                    progressesStateFlow.value[id]
 
-            operator fun setValue(thisObj: Any?, property: KProperty<*>, value: ProgressBarInfo?) {
-                progressesStateFlow.edit {
-                    if (value == null) remove(key = id)
-                    else put(key = id, value = value)
+                operator fun setValue(thisObj: Any?, property: KProperty<*>, value: ProgressBarInfo?) {
+                    progressesStateFlow.edit {
+                        if (value == null) remove(key = id)
+                        else put(key = id, value = value)
+                    }
                 }
             }
-        }
-
-        coroutineScope.launch {
+            require(progressBarInfo == null)
             block {
                 progressBarInfo = it
             }
