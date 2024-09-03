a7a237976c7a6aff94d8d025a33e8feb317c9b38
demidenko <devit15@yandex.ru>
2024-09-03 19:47:41 +0700

    rollback datastore version

--- cps-compose/app/build.gradle.kts
+++ cps-compose/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.demich.cps"
         targetSdk = 34
 
-        versionCode = 412
+        versionCode = 413
         versionName = "1.9.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
