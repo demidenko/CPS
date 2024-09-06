2834ee4c0b5bb392161f303df8d0d5e70773c028
demidenko <devit15@yandex.ru>
2024-09-06 23:02:38 +0700

    code ref

--- cps-compose/app/src/main/java/com/demich/cps/ui/lazylist/Ext.kt
+++ cps-compose/app/src/main/java/com/demich/cps/ui/lazylist/Ext.kt
@@ -1,6 +1,8 @@
 package com.demich.cps.ui.lazylist
 
 import androidx.compose.foundation.lazy.LazyItemScope
+import androidx.compose.foundation.lazy.LazyListItemInfo
+import androidx.compose.foundation.lazy.LazyListLayoutInfo
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
@@ -8,27 +10,46 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.demich.cps.ui.EmptyMessageBox
+import com.demich.cps.utils.subList
 
-fun LazyListState.visibleRange(requiredVisiblePart: Float = 0.5f): IntRange {
+private const val defaultVisiblePart = 0.5f
+
+fun LazyListLayoutInfo.visibleItemsInfo(
+    requiredVisiblePart: Float = defaultVisiblePart
+): List<LazyListItemInfo> {
     require(requiredVisiblePart in 0f..1f)
 
-    val visibleItems = layoutInfo.visibleItemsInfo
-    if (visibleItems.isEmpty()) return IntRange.EMPTY
+    val visibleItems = visibleItemsInfo
+    if (visibleItems.isEmpty()) return emptyList()
 
     val firstVisible = visibleItems.first().let { item ->
         val topHidden = (-item.offset).coerceAtLeast(0)
         val visiblePart = (item.size - topHidden).toFloat() / item.size
-        if (visiblePart < requiredVisiblePart) item.index + 1 else item.index
+        if (visiblePart < requiredVisiblePart) 1 else 0
     }
 
     val lastVisible = visibleItems.last().let { item ->
-        val bottomHidden = (item.offset + item.size - layoutInfo.viewportEndOffset)
+        val bottomHidden = (item.offset + item.size - viewportEndOffset)
             .coerceAtLeast(0)
         val visiblePart = (item.size - bottomHidden).toFloat() / item.size
-        if (visiblePart < requiredVisiblePart) item.index - 1 else item.index
+        if (visiblePart < requiredVisiblePart) visibleItems.lastIndex - 1 else visibleItems.lastIndex
     }
 
-    return firstVisible .. lastVisible
+    if (firstVisible > lastVisible) return emptyList()
+    return visibleItems.subList(firstVisible .. lastVisible)
+}
+
+fun LazyListState.visibleRange(requiredVisiblePart: Float = defaultVisiblePart): IntRange {
+    require(requiredVisiblePart in 0f..1f)
+
+    with(layoutInfo.visibleItemsInfo(requiredVisiblePart)) {
+        if (isEmpty()) return IntRange.EMPTY
+
+        val firstVisibleIndex = first().index
+        val lastVisibleIndex = last().index
+
+        return firstVisibleIndex .. lastVisibleIndex
+    }
 }
 
 
