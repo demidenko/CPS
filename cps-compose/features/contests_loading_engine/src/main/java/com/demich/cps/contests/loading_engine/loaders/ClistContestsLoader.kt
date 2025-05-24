6bb173d7a8d1615d02965fda923ae1c052a1f235
demidenko <devit15@yandex.ru>
2025-05-24 23:12:01 +0700

    code ref

--- cps-compose/features/contests_loading_engine/src/main/java/com/demich/cps/contests/loading_engine/loaders/ClistContestsLoader.kt
+++ cps-compose/features/contests_loading_engine/src/main/java/com/demich/cps/contests/loading_engine/loaders/ClistContestsLoader.kt
@@ -3,10 +3,10 @@ package com.demich.cps.contests.loading_engine.loaders
 import com.demich.cps.contests.database.Contest
 import com.demich.cps.contests.loading.ContestDateConstraints
 import com.demich.cps.contests.loading.ContestsLoaderType
-import com.demich.cps.platforms.utils.ClistUtils
 import com.demich.cps.platforms.api.ClistApi
 import com.demich.cps.platforms.api.ClistContest
 import com.demich.cps.platforms.api.ClistResource
+import com.demich.cps.platforms.utils.ClistUtils
 import kotlinx.datetime.Instant
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalTime
@@ -30,26 +30,27 @@ class ClistContestsLoader(
 }
 
 
-private fun Collection<ClistContest>.mapAndFilterResult(dateConstraints: ContestDateConstraints) =
-    mapNotNull { clistContest ->
-        val contest = clistContest.toContest()
-        if (!dateConstraints.check(startTime = contest.startTime, duration = contest.duration)) return@mapNotNull null
+private fun Collection<ClistContest>.mapAndFilterResult(dateConstraints: ContestDateConstraints): List<Contest> {
+    val format = DateTimeComponents.Format {
+        //YYYY-MM-DDThh:mm:ss
+        date(LocalDate.Formats.ISO)
+        char('T')
+        time(LocalTime.Formats.ISO)
+    }
+
+    return mapNotNull { clistContest ->
+        val contest = clistContest.toContest { Instant.parse(it, format) }
+        if (!dateConstraints.check(startTime = contest.startTime, duration = contest.duration)) {
+            return@mapNotNull null
+        }
         when (contest.platform) {
             Contest.Platform.atcoder -> contest.takeIf { clistContest.host == "atcoder.jp" }
             else -> contest
         }
     }
-
-private val contestDateTimeFormat by lazy {
-    //YYYY-MM-DDThh:mm:ss
-    DateTimeComponents.Format {
-        date(LocalDate.Formats.ISO)
-        char('T')
-        time(LocalTime.Formats.ISO)
-    }
 }
 
-private fun ClistContest.toContest(): Contest {
+private inline fun ClistContest.toContest(parseDate: (String) -> Instant): Contest {
     val platform = Contest.platformsExceptUnknown
         .find { ClistUtils.getClistApiResourceId(it) == resource_id }
         ?: Contest.Platform.unknown
@@ -58,8 +59,8 @@ private fun ClistContest.toContest(): Contest {
         platform = platform,
         id = ClistUtils.extractContestId(this, platform),
         title = event,
-        startTime = Instant.parse(start, contestDateTimeFormat),
-        endTime = Instant.parse(end, contestDateTimeFormat),
+        startTime = parseDate(start),
+        endTime = parseDate(end),
         duration = duration.seconds,
         link = href,
         host = host.takeIf { platform == Contest.Platform.unknown }
