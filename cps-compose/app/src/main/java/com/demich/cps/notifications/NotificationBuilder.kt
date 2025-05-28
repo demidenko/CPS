5ddd338c43b35a0bcd5b94aee44417560d7d96a3
demidenko <devit15@yandex.ru>
2025-05-28 22:27:55 +0700

    move class out

---
+++ cps-compose/app/src/main/java/com/demich/cps/notifications/NotificationBuilder.kt
@@ -0,0 +1,31 @@
+package com.demich.cps.notifications
+
+import android.Manifest
+import android.app.Notification
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
+class NotificationBuilder internal constructor(
+    private val builder: NotificationCompat.Builder,
+    val notificationId: Int,
+    private val notificationManager: NotificationManagerCompat,
+    private val context: Context
+) {
+    fun edit(block: NotificationCompat.Builder.() -> Unit) = builder.block()
+
+    fun build(): Notification = builder.build()
+
+    @JvmName("notifyCustom")
+    fun notify() {
+        if (!notificationManager.areNotificationsEnabled()) return
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED)
+                return
+        }
+        notificationManager.notify(notificationId, build())
+    }
+}
--- cps-compose/app/src/main/java/com/demich/cps/notifications/NotificationChannel.kt
+++ cps-compose/app/src/main/java/com/demich/cps/notifications/NotificationChannel.kt
@@ -1,53 +1,27 @@
 package com.demich.cps.notifications
 
-import android.Manifest
-import android.app.Notification
 import android.content.Context
-import android.content.pm.PackageManager
-import android.os.Build
-import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
-
-class NotificationBuilder internal constructor(
-    private val builder: NotificationCompat.Builder,
-    val notificationId: Int,
-    private val notificationManager: NotificationManagerCompat,
-    private val context: Context
-) {
-    fun edit(buildBody: NotificationCompat.Builder.() -> Unit) = builder.buildBody()
-
-    fun build(): Notification = builder.build()
-
-    @JvmName("notifyCustom")
-    fun notify() {
-        if (!notificationManager.areNotificationsEnabled()) return
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED)
-                return
-        }
-        notificationManager.notify(notificationId, build())
-    }
-}
 
 class NotificationChannelSingleId internal constructor(
     val notificationId: Int,
     val channelInfo: NotificationChannelInfo
 ) {
-    fun builder(context: Context, buildBody: NotificationCompat.Builder.() -> Unit): NotificationBuilder {
+    fun builder(context: Context, block: NotificationCompat.Builder.() -> Unit): NotificationBuilder {
         val notificationManager = NotificationManagerCompat.from(context).apply {
             createNotificationChannel(channelInfo)
         }
         return NotificationBuilder(
-            builder = NotificationCompat.Builder(context, channelInfo.id).apply(buildBody),
+            builder = NotificationCompat.Builder(context, channelInfo.id),
             notificationId = notificationId,
             notificationManager = notificationManager,
             context = context
-        )
+        ).apply { edit(block) }
     }
 
-    fun notify(context: Context, buildBody: NotificationCompat.Builder.() -> Unit) =
-        builder(context, buildBody).notify()
+    fun notify(context: Context, block: NotificationCompat.Builder.() -> Unit) =
+        builder(context, block).notify()
 }
 
 class NotificationChannelRangeId internal constructor(
