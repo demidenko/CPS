988e2efc091eb5623279339803471cc2e0af8868
demidenko <devit15@yandex.ru>
2025-06-02 20:37:27 +0700

    remove inline

--- cps-compose/app/src/main/java/com/demich/cps/ui/CPSUI.kt
+++ cps-compose/app/src/main/java/com/demich/cps/ui/CPSUI.kt
@@ -332,9 +332,9 @@ fun CPSCountBadge(count: Int) {
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
-inline fun CPSSwipeRefreshBox(
-    noinline isRefreshing: () -> Boolean,
-    noinline onRefresh: () -> Unit,
+fun CPSSwipeRefreshBox(
+    isRefreshing: () -> Boolean,
+    onRefresh: () -> Unit,
     modifier: Modifier = Modifier,
     content: @Composable () -> Unit
 ) {
