aeb102883bf7c8375d06c5c1c64e699393a0bf3e
demidenko <devit15@yandex.ru>
2025-06-06 10:57:52 +0700

    dummy animated subtitle

--- cps-compose/app/src/main/java/com/demich/cps/ui/topbar/Title.kt
+++ cps-compose/app/src/main/java/com/demich/cps/ui/topbar/Title.kt
@@ -1,10 +1,16 @@
 package com.demich.cps.ui.topbar
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.ProvideTextStyle
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
@@ -29,11 +35,33 @@ internal fun Title(
                 color = cpsColors.content,
                 maxLines = 1
             )
-            Text(
-                text = subtitle(),
-                color = cpsColors.contentAdditional,
-                maxLines = 1
+            SubTitle(
+                text = subtitle,
+                color = cpsColors.contentAdditional
             )
+        }
+    }
+}
+
+@Composable
+internal fun SubTitle(
+    text: () -> String,
+    color: Color
+) {
+    //TODO: improve animation by longest common subseq
+
+    val prefixes by remember(text) {
+        derivedStateOf {
+            text().runningFold("") { pref, it -> pref + it }.drop(1)
+        }
+    }
+
+    LazyRow {
+        items(
+            items = prefixes,
+            key = { it }
+        ) {
+            Text(text = "${it.last()}", color = color, modifier = Modifier.animateItem())
         }
     }
 }
