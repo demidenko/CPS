221847e313463287731685f2c70d37f0f73991dc
demidenko <devit15@yandex.ru>
2025-06-09 19:52:32 +0700

    upd version + build

--- cps-compose/app/build.gradle.kts
+++ cps-compose/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.demich.cps"
         targetSdk = 35
 
-        versionCode = 447
+        versionCode = 448
         versionName = "1.9.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
