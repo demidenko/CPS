b1301ca03d9384f54d1c4c747e20a4e351cbc20a
demidenko <devit15@yandex.ru>
2025-06-18 02:35:30 +0700

    userInfoSerializersModule

--- cps-compose/data/accounts/userinfo/src/main/java/com/demich/cps/accounts/userinfo/ProfileResult.kt
+++ cps-compose/data/accounts/userinfo/src/main/java/com/demich/cps/accounts/userinfo/ProfileResult.kt
@@ -1,17 +1,42 @@
 package com.demich.cps.accounts.userinfo
 
-// TODO: @Serializable
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.polymorphic
+import kotlinx.serialization.modules.subclass
+
+@Serializable
 sealed interface ProfileResult<out U: UserInfo> {
     val userId: String
 
+    @Serializable
     data class NotFound(override val userId: String): ProfileResult<Nothing>
 
+    @Serializable
     data class Failed(override val userId: String): ProfileResult<Nothing>
 
+    @Serializable
     data class Success<U: UserInfo>(val userInfo: U): ProfileResult<U> {
         override val userId: String
             get() = userInfo.userId
     }
+}
+
+private val userInfoSerializersModule = SerializersModule {
+    polymorphic(UserInfo::class) {
+        subclass(ACMPUserInfo::class)
+        subclass(AtCoderUserInfo::class)
+        subclass(CodeChefUserInfo::class)
+        subclass(CodeforcesUserInfo::class)
+        subclass(DmojUserInfo::class)
+        subclass(TimusUserInfo::class)
+    }
+}
+
+val jsonProfile = Json {
+    ignoreUnknownKeys = true
+    serializersModule = userInfoSerializersModule
 }
 
 fun <U: UserInfo> ProfileResult<U>.userInfoOrNull(): U? =
