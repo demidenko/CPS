9f74695dfe0dd4723aebb71870c30f29a48be6c1
demidenko <devit15@yandex.ru>
2025-06-26 05:38:33 +0700

    add synchronized (based on datastore delegate)

--- cps-compose/features/room_base/src/main/java/com/demich/cps/features/room/InstanceProvider.kt
+++ cps-compose/features/room_base/src/main/java/com/demich/cps/features/room/InstanceProvider.kt
@@ -1,33 +1,34 @@
 package com.demich.cps.features.room
 
 import android.content.Context
+import androidx.annotation.GuardedBy
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.migration.Migration
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
-abstract class InstanceProvider<T: RoomDatabase>(
-    private val builder: (Context) ->  RoomDatabase.Builder<T>
-) {
-    private var instance: T? = null
-    fun getInstance(context: Context): T {
-        return instance ?: builder(context).build().also { instance = it }
-    }
-}
 
 inline fun <reified D: RoomDatabase> instanceDelegate(
     name: String,
     crossinline migrations: () -> List<Migration> = { emptyList() }
 ): ReadOnlyProperty<Context, D> =
     object : ReadOnlyProperty<Context, D> {
+        // based on PreferenceDataStoreSingletonDelegate
+        private val lock = Any()
+
+        @GuardedBy("lock")
+        @Volatile
         private var instance: D? = null
+
         override fun getValue(thisRef: Context, property: KProperty<*>): D {
-            instance?.let { return it }
-
-            val builder = Room.databaseBuilder<D>(context = thisRef, name = name)
-                .addMigrations(migrations = migrations().toTypedArray())
-
-            return builder.build().also { instance = it }
+            return instance ?: synchronized(lock) {
+                if (instance == null) {
+                    val builder = Room.databaseBuilder<D>(context = thisRef, name = name)
+                        .addMigrations(migrations = migrations().toTypedArray())
+                    instance = builder.build()
+                }
+                requireNotNull(instance)
+            }
         }
     }
