ce93a8ceb35620fe1f5dd3c0b670b8378225272a
demidenko <devit15@yandex.ru>
2025-06-25 21:14:05 +0700

    remove internal modifier

--- cps-compose/data/platforms/api/src/main/java/com/demich/cps/platforms/api/codeforces/CodeforcesApiException.kt
+++ cps-compose/data/platforms/api/src/main/java/com/demich/cps/platforms/api/codeforces/CodeforcesApiException.kt
@@ -1,22 +1,15 @@
 package com.demich.cps.platforms.api.codeforces
 
-open class CodeforcesApiException
-internal constructor(comment: String): Throwable(message = comment)
+open class CodeforcesApiException(comment: String): Throwable(message = comment)
 
-class CodeforcesApiCallLimitExceededException
-internal constructor(comment: String): CodeforcesApiException(comment)
+class CodeforcesApiCallLimitExceededException(comment: String): CodeforcesApiException(comment)
 
-class CodeforcesApiHandleNotFoundException
-internal constructor(comment: String, val handle: String): CodeforcesApiException(comment)
+class CodeforcesApiHandleNotFoundException(comment: String, val handle: String): CodeforcesApiException(comment)
 
-class CodeforcesApiNotAllowedReadBlogException
-internal constructor(comment: String): CodeforcesApiException(comment)
+class CodeforcesApiNotAllowedReadBlogException(comment: String): CodeforcesApiException(comment)
 
-class CodeforcesApiContestRatingUnavailableException
-internal constructor(comment: String): CodeforcesApiException(comment)
+class CodeforcesApiContestRatingUnavailableException(comment: String): CodeforcesApiException(comment)
 
-class CodeforcesApiContestNotStartedException
-internal constructor(comment: String, val contestId: Int): CodeforcesApiException(comment)
+class CodeforcesApiContestNotStartedException(comment: String, val contestId: Int): CodeforcesApiException(comment)
 
-class CodeforcesApiContestNotFoundException
-internal constructor(comment: String, val contestId: Int): CodeforcesApiException(comment)
+class CodeforcesApiContestNotFoundException(comment: String, val contestId: Int): CodeforcesApiException(comment)
