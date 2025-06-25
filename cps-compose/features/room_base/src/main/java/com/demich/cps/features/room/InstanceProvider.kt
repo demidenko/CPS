22827de9328f2ed0ec6aee6501dea4deda3e1d6e
demidenko <devit15@yandex.ru>
2025-06-26 05:07:01 +0700

    instanceDelegate

--- cps-compose/features/room_base/src/main/java/com/demich/cps/features/room/InstanceProvider.kt
+++ cps-compose/features/room_base/src/main/java/com/demich/cps/features/room/InstanceProvider.kt
@@ -1,7 +1,10 @@
 package com.demich.cps.features.room
 
 import android.content.Context
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
 
 abstract class InstanceProvider<T: RoomDatabase>(
     private val builder: (Context) ->  RoomDatabase.Builder<T>
@@ -11,3 +14,13 @@ abstract class InstanceProvider<T: RoomDatabase>(
         return instance ?: builder(context).build().also { instance = it }
     }
 }
+
+inline fun <reified D: RoomDatabase> instanceDelegate(
+    name: String
+): ReadOnlyProperty<Context, D> =
+    object : ReadOnlyProperty<Context, D> {
+        private var instance: D? = null
+        override fun getValue(thisRef: Context, property: KProperty<*>): D {
+            return instance ?: Room.databaseBuilder<D>(context = thisRef, name = name).build().also { instance = it }
+        }
+    }
