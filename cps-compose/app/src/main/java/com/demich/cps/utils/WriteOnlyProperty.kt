6c8f59f1821218b661217a2615682eca455348f4
demidenko <devit15@yandex.ru>
2025-07-05 16:43:37 +0700

    remove receiver

--- cps-compose/app/src/main/java/com/demich/cps/utils/WriteOnlyProperty.kt
+++ cps-compose/app/src/main/java/com/demich/cps/utils/WriteOnlyProperty.kt
@@ -3,13 +3,13 @@ package com.demich.cps.utils
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
-inline fun <C, T> C.writeOnlyProperty(crossinline set: C.(T) -> Unit): ReadWriteProperty<C, T> =
-    object : ReadWriteProperty<C, T> {
-        override fun getValue(thisRef: C, property: KProperty<*>): T {
+inline fun <T> writeOnlyProperty(crossinline set: (T) -> Unit): ReadWriteProperty<Any?, T> =
+    object : ReadWriteProperty<Any?, T> {
+        override fun getValue(thisRef: Any?, property: KProperty<*>): T {
             error("get in write-only delegate")
         }
 
-        override fun setValue(thisRef: C, property: KProperty<*>, value: T) {
+        override fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
             set(value)
         }
     }
