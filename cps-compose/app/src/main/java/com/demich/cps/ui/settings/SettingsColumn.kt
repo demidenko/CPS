c8da02de20591169863056ed1fa6a702deddbea4
demidenko <devit15@yandex.ru>
2025-07-08 22:19:45 +0700

    Revert "column instead of box"
    This reverts commit 00d2415a5991173f1da8385e1fe4361c13b90e6a.

--- cps-compose/app/src/main/java/com/demich/cps/ui/settings/SettingsColumn.kt
+++ cps-compose/app/src/main/java/com/demich/cps/ui/settings/SettingsColumn.kt
@@ -2,6 +2,7 @@ package com.demich.cps.ui.settings
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,6 +11,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -50,12 +52,12 @@ inline fun SettingsColumn(
 object SettingsColumnScopeInstance: SettingsContainerScope {
     @Composable
     override fun append(content: @Composable () -> Unit) {
-        Column(
-            verticalArrangement = Arrangement.spacedBy(10.dp),
+        Box(
             modifier = Modifier
                 .background(cpsColors.backgroundAdditional, RoundedCornerShape(4.dp))
                 .fillMaxWidth()
                 .padding(all = 10.dp),
+            contentAlignment = Alignment.CenterStart
         ) {
             content()
         }
