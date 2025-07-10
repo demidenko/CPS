385417669f24ada896de7985c0283d426b37d702
demidenko <devit15@yandex.ru>
2025-07-10 17:58:23 +0700

    change PermissionPanel

--- cps-compose/app/src/main/java/com/demich/cps/ui/Permissions.kt
+++ cps-compose/app/src/main/java/com/demich/cps/ui/Permissions.kt
@@ -6,18 +6,25 @@ import android.Manifest
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
-import androidx.compose.material.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.demich.cps.ui.theme.CPSTheme
 import com.demich.cps.ui.theme.cpsColors
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
@@ -42,25 +49,11 @@ fun NotificationsPermissionPanel(
 private fun NotificationsPermissionPanel(modifier: Modifier) {
     val state = rememberPermissionState(permission = Manifest.permission.POST_NOTIFICATIONS)
     if (!state.status.isGranted) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = modifier
-                .background(color = cpsColors.error)
-                .padding(all = 8.dp)
-        ) {
-            Text(
-                text = "Some features you turned on use notifications for full functionality. Please grant the permission for CPS to post notifications.",
-                fontWeight = FontWeight.SemiBold,
-                color = contentColorFor(cpsColors.error)
-            )
-            OutlinedButton(
-                onClick = { state.launchPermissionRequest() },
-                content = { Text("Request permission") },
-                colors = ButtonDefaults.buttonColors(
-                    backgroundColor = cpsColors.backgroundAdditional
-                )
-            )
-        }
+        PermissionPanel(
+            modifier = modifier,
+            message = "Some features you turned on use notifications for full functionality. Please grant the permission for CPS to post notifications.",
+            onClick = { state.launchPermissionRequest() },
+        )
     }
 
     DisposableEffect(state) {
@@ -68,5 +61,56 @@ private fun NotificationsPermissionPanel(modifier: Modifier) {
             state.launchPermissionRequest()
         }
         onDispose {  }
+    }
+}
+
+@Composable
+private fun PermissionPanel(
+    modifier: Modifier = Modifier,
+    message: String,
+    onClick: () -> Unit
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .background(color = cpsColors.backgroundAdditional, shape = RoundedCornerShape(4.dp))
+            .border(color = cpsColors.error, width = 1.dp, shape = RoundedCornerShape(4.dp))
+            .padding(all = 8.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            IconSp(
+                imageVector = CPSIcons.Attention,
+                size = 19.sp,
+                color = cpsColors.error
+            )
+            Text(
+                text = message,
+                fontWeight = FontWeight.SemiBold,
+                color = cpsColors.error,
+                modifier = Modifier.weight(1f)
+            )
+        }
+
+        OutlinedButton(
+            onClick = onClick,
+            content = { Text(text = "Request permission") },
+            colors = ButtonDefaults.outlinedButtonColors(
+                contentColor = cpsColors.content
+            )
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewPermissionPanel() {
+    CPSTheme {
+        PermissionPanel(
+            message = "Please grand permission for full functionality",
+            onClick = { }
+        )
     }
 }
