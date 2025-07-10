1d334662ced58d52b8bf327b7359c6635c352b45
demidenko <devit15@yandex.ru>
2025-07-11 03:30:23 +0700

    use enumset

--- cps-compose/data/contests/loading/src/main/java/com/demich/cps/contests/loading/ContestsLoaderType.kt
+++ cps-compose/data/contests/loading/src/main/java/com/demich/cps/contests/loading/ContestsLoaderType.kt
@@ -1,9 +1,10 @@
 package com.demich.cps.contests.loading
 
 import com.demich.cps.contests.database.Contest
+import java.util.EnumSet
 
 enum class ContestsLoaderType(val supportedPlatforms: Set<Contest.Platform>) {
-    clist_api(Contest.platforms.toSet()),
+    clist_api(EnumSet.copyOf(Contest.platforms)),
     codeforces_api(Contest.Platform.codeforces),
     atcoder_parse(Contest.Platform.atcoder),
     dmoj_api(Contest.Platform.dmoj)
