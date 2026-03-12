9eb01bc7b9e0762be806373d8d8ab2d14a7c8e6d
demidenko <devit15@yandex.ru>
2026-03-13 06:46:22 +0700

    todo rename

--- cps-compose/data/contests/loading/src/main/java/com/demich/cps/contests/loading/ContestsLoaderType.kt
+++ cps-compose/data/contests/loading/src/main/java/com/demich/cps/contests/loading/ContestsLoaderType.kt
@@ -3,6 +3,7 @@ package com.demich.cps.contests.loading
 import com.demich.cps.contests.database.Contest
 import com.demich.kotlin_stdlib_boost.toEnumSet
 
+//TODO: proper name
 enum class ContestsLoaderType(val supportedPlatforms: Set<Contest.Platform>) {
     clist_api(Contest.platforms.toEnumSet()),
     codeforces_api(Contest.Platform.codeforces),
