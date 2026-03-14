a5d3b5305dd539800324a03d53c420fd754b4f74
demidenko <devit15@yandex.ru>
2026-03-15 05:27:36 +0700

    add rememberWithFirst

--- cps-compose/app/src/main/java/com/demich/cps/utils/CollectAsState.kt
+++ cps-compose/app/src/main/java/com/demich/cps/utils/CollectAsState.kt
@@ -25,27 +25,34 @@ fun <T> DataStoreValue<T>.getValueBlocking(): T =
 
 @Composable
 inline fun <T> collectAsState(crossinline block: () -> Flow<T>): State<T> =
-    remember(calculation = block).let { flow ->
-        flow.collectAsState(initial = remember { flow.firstBlocking() })
+    rememberWithFirst(block = block).let { (flow, value) ->
+        flow.collectAsState(initial = value)
     }
 
 @Composable
 inline fun <T> collectAsStateWithLifecycle(crossinline block: () -> Flow<T>): State<T> =
-    remember(calculation = block).let { flow ->
-        flow.collectAsStateWithLifecycle(initialValue = remember { flow.firstBlocking() })
+    rememberWithFirst(block = block).let { (flow, value) ->
+        flow.collectAsStateWithLifecycle(initialValue = value)
     }
 
 @Composable
 inline fun <T> collectItemAsState(
     crossinline block: () -> DataStoreValue<T>
 ): State<T> =
-    remember {
-        block().run { asFlow() to getValueBlocking() }
-    }.run { first.collectAsState(initial = second) }
+    remember { block().run { asFlow() to getValueBlocking() } }.let { (flow, value) ->
+        flow.collectAsState(initial = value)
+    }
 
 @Composable
 inline fun <T> rememberFirst(crossinline block: () -> Flow<T>): T =
     remember { block().firstBlocking() }
+
+@Composable
+inline fun <T> rememberWithFirst(crossinline block: () -> Flow<T>): Pair<Flow<T>, T> =
+    remember {
+        val flow = block()
+        flow to flow.firstBlocking()
+    }
 
 @Composable
 inline fun <T> rememberFirstValue(crossinline block: () -> DataStoreValue<T>): T =
