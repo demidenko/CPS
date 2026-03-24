efedaf528269e8de97c95df7e4a7e830fbef813a
demidenko <devit15@yandex.ru>
2026-03-25 03:15:20 +0700

    fix negative timer

--- cps-compose/app/src/main/java/com/demich/cps/contests/monitors/CodeforcesMonitorNotifier.kt
+++ cps-compose/app/src/main/java/com/demich/cps/contests/monitors/CodeforcesMonitorNotifier.kt
@@ -10,10 +10,9 @@ import com.demich.cps.R
 import com.demich.cps.notifications.NotificationBuilder
 import com.demich.cps.platforms.api.codeforces.models.CodeforcesContestType
 import com.demich.cps.platforms.api.codeforces.models.title
-import com.demich.cps.utils.getSystemTime
 import kotlin.properties.ObservableProperty
 import kotlin.reflect.KProperty
-import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Clock
 
 class CodeforcesMonitorNotifier(
     val context: Context,
@@ -77,9 +76,9 @@ class CodeforcesMonitorNotifier(
     ) { oldPhase, phase ->
         views.forEach { it.setTextViewText(R.id.cf_monitor_phase, phase.phase.title) }
         if (phase is CodeforcesMonitorData.ContestPhase.Coding) {
-            val remaining = phase.endTime - getSystemTime()
-            val elapsed = SystemClock.elapsedRealtime().milliseconds
-            views.forEach { it.setChronometer(R.id.cf_monitor_progress, (elapsed + remaining).inWholeMilliseconds, null, true) }
+            val remainingMs = (phase.endTime - Clock.System.now()).inWholeMilliseconds.coerceAtLeast(0)
+            val elapsedMs = SystemClock.elapsedRealtime()
+            views.forEach { it.setChronometer(R.id.cf_monitor_progress, elapsedMs + remainingMs, null, true) }
         } else {
             if (phase.phase != oldPhase.phase) {
                 views.forEach { it.setChronometer(R.id.cf_monitor_progress, 0, null, false) }
