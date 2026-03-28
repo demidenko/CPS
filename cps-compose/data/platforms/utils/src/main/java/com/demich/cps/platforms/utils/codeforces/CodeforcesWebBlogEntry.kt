25930dac7474e63db4d71a0c2279e452333cf7e2
demidenko <devit15@yandex.ru>
2026-03-29 02:20:37 +0700

    add blogEntry mapper

--- cps-compose/data/platforms/utils/src/main/java/com/demich/cps/platforms/utils/codeforces/CodeforcesWebBlogEntry.kt
+++ cps-compose/data/platforms/utils/src/main/java/com/demich/cps/platforms/utils/codeforces/CodeforcesWebBlogEntry.kt
@@ -1,9 +1,12 @@
 package com.demich.cps.platforms.utils.codeforces
 
 import com.demich.cps.platforms.api.codeforces.models.CodeforcesBlogEntry
+import com.demich.cps.platforms.api.codeforces.models.CodeforcesColorTag
+import kotlinx.serialization.Serializable
 import org.jsoup.Jsoup
 import kotlin.time.Instant
 
+@Serializable
 data class CodeforcesWebBlogEntry(
     val id: Int,
     val title: String,
@@ -15,3 +18,13 @@ data class CodeforcesWebBlogEntry(
 
 fun CodeforcesBlogEntry.extractTitle(): String =
     Jsoup.parse(title).text()
+
+fun CodeforcesBlogEntry.toWebBlogEntry(colorTag: CodeforcesColorTag) =
+    CodeforcesWebBlogEntry(
+        id = id,
+        title = title,
+        author = CodeforcesHandle(handle = authorHandle, colorTag = colorTag),
+        creationTime = creationTime,
+        rating = rating,
+        commentsCount = 0
+    )
