871dcb7fdd19311e7561482748945ddc1f6442c1
demidenko <devit15@yandex.ru>
2026-03-30 15:34:19 +0700

    code ref

--- cps-compose/app/src/main/java/com/demich/cps/community/follow/CodeforcesFollowRepositoryImpl.kt
+++ cps-compose/app/src/main/java/com/demich/cps/community/follow/CodeforcesFollowRepositoryImpl.kt
@@ -7,16 +7,24 @@ import com.demich.cps.features.codeforces.follow.database.CodeforcesFollowReposi
 import com.demich.cps.notifications.notificationChannels
 import com.demich.cps.platforms.api.codeforces.CodeforcesUrls
 import com.demich.cps.platforms.api.codeforces.models.CodeforcesBlogEntry
+import com.demich.cps.platforms.api.codeforces.models.CodeforcesLocale
 import com.demich.cps.platforms.clients.codeforces.CodeforcesClient
 import com.demich.cps.platforms.utils.codeforces.extractTitle
+import com.demich.datastore_itemized.DataStoreValue
 
 val Context.followRepository: CodeforcesFollowRepository
-    get() = CodeforcesFollowRepositoryImpl(context = this)
+    get() = CodeforcesFollowRepositoryImpl(
+        context = this,
+        localeItem = settingsCommunity.codeforcesLocale
+    )
 
-private class CodeforcesFollowRepositoryImpl(val context: Context):
+private class CodeforcesFollowRepositoryImpl(
+    val context: Context, //TODO: context leak
+    val localeItem: DataStoreValue<CodeforcesLocale>
+):
     CodeforcesFollowRepository(api = CodeforcesClient, context = context) {
 
-    override suspend fun getLocale() = context.settingsCommunity.codeforcesLocale()
+    override suspend fun getLocale() = localeItem()
 
     override fun notifyNewBlogEntry(blogEntry: CodeforcesBlogEntry) =
         notificationChannels.codeforces.new_blog_entry(blogEntry.id).notify(context) {
