b57b81e7baa479bab1b6baffcba672d41579618e
demidenko <devit15@yandex.ru>
2026-04-02 20:44:25 +0700

    code ref

--- cps-compose/data/platforms/utils/src/main/java/com/demich/cps/platforms/utils/codeforces/GetRecentFeed.kt
+++ cps-compose/data/platforms/utils/src/main/java/com/demich/cps/platforms/utils/codeforces/GetRecentFeed.kt
@@ -4,34 +4,30 @@ import com.demich.cps.platforms.api.codeforces.CodeforcesPageContentProvider
 import com.demich.cps.platforms.api.codeforces.models.CodeforcesLocale
 import kotlin.math.max
 
-suspend fun CodeforcesPageContentProvider.getRecentFeed(locale: CodeforcesLocale): CodeforcesRecentFeed {
-    val source = getRecentActionsPage(locale = locale)
-    val comments = CodeforcesUtils.extractComments(source)
+suspend fun CodeforcesPageContentProvider.getRecentFeed(locale: CodeforcesLocale): CodeforcesRecentFeed =
+    CodeforcesUtils.extractRecentFeed(source = getRecentActionsPage(locale = locale))
+
+private fun CodeforcesUtils.extractRecentFeed(source: String): CodeforcesRecentFeed {
+    val comments = extractComments(source)
     //blog entry with low rating disappeared from blogEntries but has comments, need to merge
-    val blogEntries = CodeforcesUtils.extractRecentBlogEntries(source).toMutableList()
+    val blogEntries = extractRecentBlogEntries(source).toMutableList()
     val blogEntriesIds = blogEntries.mapTo(mutableSetOf()) { it.id }
     val usedIds = mutableSetOf<Int>()
     var index = 0
     for (comment in comments) {
-        val blogEntry = CodeforcesRecentFeedBlogEntry(
-            id = comment.blogEntryId,
-            title = comment.blogEntryTitle,
-            author = comment.blogEntryAuthor,
-            isLowRated = false
-        )
-        val id = blogEntry.id
+        val id = comment.blogEntryId
         if (id !in blogEntriesIds) {
             blogEntriesIds.add(id)
             if (index < blogEntries.size) {
                 //mark low rated
                 blogEntries.add(
                     index = index,
-                    element = blogEntry.copy(isLowRated = true)
+                    element = comment.toRecentFeedBlogEntry(isLowRated = true)
                 )
             } else {
                 //latest recent comments has no blog entries in recent action, so most likely not low rated
                 check(index == blogEntries.size)
-                blogEntries.add(blogEntry)
+                blogEntries.add(comment.toRecentFeedBlogEntry(isLowRated = false))
             }
         }
         if (id !in usedIds) {
@@ -42,3 +38,11 @@ suspend fun CodeforcesPageContentProvider.getRecentFeed(locale: CodeforcesLocale
     }
     return CodeforcesRecentFeed(blogEntries, comments)
 }
+
+private fun CodeforcesWebComment.toRecentFeedBlogEntry(isLowRated: Boolean) =
+    CodeforcesRecentFeedBlogEntry(
+        id = blogEntryId,
+        title = blogEntryTitle,
+        author = blogEntryAuthor,
+        isLowRated = isLowRated
+    )
