8f7964963ca1c635fa9bc8328018d5c54b6b2a0f
demidenko <devit15@yandex.ru>
2026-04-24 05:42:35 +0700

    add isShrinkResources = true

--- cps-compose/app/build.gradle.kts
+++ cps-compose/app/build.gradle.kts
@@ -24,6 +24,7 @@ configure<ApplicationExtension> {
     buildTypes {
         release {
             isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
