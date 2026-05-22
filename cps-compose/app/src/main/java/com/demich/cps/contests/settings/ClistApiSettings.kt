d95b09230ee6059a251515369832f4e4170e2f63
demidenko <devit15@yandex.ru>
2026-05-22 17:49:31 +0700

    use general api dialog item

--- cps-compose/app/src/main/java/com/demich/cps/contests/settings/ClistApiSettings.kt
+++ cps-compose/app/src/main/java/com/demich/cps/contests/settings/ClistApiSettings.kt
@@ -1,38 +1,19 @@
 package com.demich.cps.contests.settings
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.demich.cps.platforms.api.clist.ClistApiAccess
 import com.demich.cps.platforms.api.clist.ClistUrls
-import com.demich.cps.ui.CPSDefaults
-import com.demich.cps.ui.CPSIconButton
-import com.demich.cps.ui.CPSIcons
-import com.demich.cps.ui.dialogs.CPSDialog
-import com.demich.cps.ui.dialogs.CPSDialogCancelAcceptButtons
+import com.demich.cps.ui.settings.ApiAccessSettingsItem
 import com.demich.cps.ui.settings.SettingsContainerScope
-import com.demich.cps.ui.settings.SubtitledByValue
 import com.demich.cps.ui.theme.cpsColors
 import com.demich.cps.utils.append
 import com.demich.cps.utils.context
 import com.demich.cps.utils.openUrlInBrowser
-import com.demich.cps.utils.rememberFirstValue
 import com.demich.datastore_itemized.edit
 import com.demich.datastore_itemized.value
 import kotlinx.coroutines.launch
@@ -42,110 +23,43 @@ context(scope: SettingsContainerScope)
 internal fun ClistApiAccessSettingsItem(
     settings: ContestsSettingsDataStore
 ) {
-    var showDialog by rememberSaveable { mutableStateOf(false) }
-    SubtitledByValue(
-        modifier = Modifier.clickable { showDialog = true },
-        item = settings.clistApiAccess,
-        title = "Clist API access"
-    ) { (login, key) ->
-        if (login.isBlank()) {
-            Text(text = "click to setup")
-        } else {
-            Text(
-                text = buildAnnotatedString {
-                    append(login)
-                    if (key.isBlank()) append(" [api-key is empty]", color = cpsColors.error)
-                },
-                fontFamily = FontFamily.Monospace,
-                color = cpsColors.content,
-                fontSize = 13.sp
-            )
-        }
-    }
-
-    if (showDialog) {
-        ClistApiDialog(
-            settings = settings,
-            onDismissRequest = { showDialog = false }
-        )
-    }
-}
-
-@Composable
-private fun ClistApiDialog(
-    settings: ContestsSettingsDataStore,
-    onDismissRequest: () -> Unit
-) {
     val context = context
     val scope = rememberCoroutineScope()
 
-    val initApiAccess = rememberFirstValue { settings.clistApiAccess }
-    var login by rememberSaveable { mutableStateOf(initApiAccess.login) }
-    var key by rememberSaveable { mutableStateOf(initApiAccess.key) }
-
-    CPSDialog(onDismissRequest = onDismissRequest) {
-        Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
-            Text(
-                text = "clist.by::api",
-                style = CPSDefaults.MonospaceTextStyle,
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(start = 4.dp)
-            )
-            CPSIconButton(icon = CPSIcons.Help) {
-                context.openUrlInBrowser(ClistUrls.apiHelp)
+    ApiAccessSettingsItem(
+        item = settings.clistApiAccess,
+        itemTitle = "Clist API access",
+        itemSubtitle = { (login, key) ->
+            if (login.isBlank()) {
+                Text(text = "click to setup")
+            } else {
+                Text(
+                    text = buildAnnotatedString {
+                        append(login)
+                        if (key.isBlank()) append(" [api-key is empty]", color = cpsColors.error)
+                    },
+                    fontFamily = FontFamily.Monospace,
+                    color = cpsColors.content,
+                    fontSize = 13.sp
+                )
             }
-        }
-
-        ClistApiTextField(
-            input = login,
-            onChangeInput = { login = it },
-            title = "login",
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 8.dp)
-        )
-
-        ClistApiTextField(
-            input = key,
-            onChangeInput = { key = it },
-            title = "api-key",
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 8.dp)
-        )
-
-        CPSDialogCancelAcceptButtons(
-            acceptTitle = "Save",
-            onCancelClick = onDismissRequest,
-            modifier = Modifier.padding(top = 8.dp)
-        ) {
+        },
+        dialogTitle = "clist.by::api",
+        fields = listOf(
+            "login" to ClistApiAccess::login,
+            "api-key" to ClistApiAccess::key,
+        ),
+        onSave = { values ->
             scope.launch {
+                val (login, key) = values
                 settings.edit {
                     clistApiLogin.value = login
                     clistApiKey.value = key
                 }
-                onDismissRequest()
             }
+        },
+        onHelp = {
+            context.openUrlInBrowser(ClistUrls.apiHelp)
         }
-    }
-}
-
-@Composable
-private fun ClistApiTextField(
-    input: String,
-    onChangeInput: (String) -> Unit,
-    title: String,
-    modifier: Modifier = Modifier,
-    inputTextSize: TextUnit = 15.sp
-) {
-    TextField(
-        modifier = modifier,
-        value = input,
-        singleLine = true,
-        textStyle = TextStyle(fontSize = inputTextSize, fontFamily = FontFamily.Monospace),
-        label = { Text(text = title, style = CPSDefaults.MonospaceTextStyle) },
-        onValueChange = onChangeInput,
-        isError = input.isBlank()
     )
 }
