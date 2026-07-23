a1e9fb8c1fd4550402be6403a388131b22c76bec
demidenko <devit15@yandex.ru>
2026-07-24 06:59:49 +0700

    add base parser

--- cps-compose/data/platforms/utils/src/main/java/com/demich/cps/platforms/utils/codeforces/CodeforcesParser.kt
+++ cps-compose/data/platforms/utils/src/main/java/com/demich/cps/platforms/utils/codeforces/CodeforcesParser.kt
@@ -1,7 +1,10 @@
 package com.demich.cps.platforms.utils.codeforces
 
+import com.demich.cps.platforms.utils.EvaluatorTagWithClass
+import com.demich.cps.platforms.utils.expectFirst
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
+import org.jsoup.select.Evaluator
 
 internal interface CodeforcesPageParser {
 
@@ -16,3 +19,19 @@ internal fun Document.selectSidebar(): Element? = selectFirst("div#sidebar")
 
 context(parser: CodeforcesPageParser)
 internal fun Document.expectSidebar(): Element = requireNotNull(selectSidebar())
+
+
+abstract class CodeforcesCommunityPageParser: CodeforcesPageParser {
+    private val evaluatorDivInfo = EvaluatorTagWithClass(tag = "div", className = "info")
+    protected fun Element.expectDivInfo(): Element = expectFirst(evaluatorDivInfo)
+
+    private val evaluatorRatedUser = Evaluator.Class("rated-user")
+    protected fun Element.selectRatedUser(): Element? = selectFirst(evaluatorRatedUser)
+    protected fun Element.expectRatedUser(): Element = expectFirst(evaluatorRatedUser)
+
+    private val evaluatorHumanTime = Evaluator.Class("format-humantime")
+    protected fun Element.expectHumanTime(): Element = expectFirst(evaluatorHumanTime)
+
+    private val evaluatorHrefBlogEntry = Evaluator.AttributeWithValueStarting("href", "/blog/entry/")
+    protected fun Element.expectBlogEntryHref(): Element = expectFirst(evaluatorHrefBlogEntry)
+}
