f05991be94fa3bbaac5154159012906b0b79d348
demidenko <devit15@yandex.ru>
2026-07-24 07:23:23 +0700

    remove cf utils

--- cps-compose/data/platforms/utils/src/main/java/com/demich/cps/platforms/utils/codeforces/CodeforcesUtils.kt
+++ cps-compose/data/platforms/utils/src/main/java/com/demich/cps/platforms/utils/codeforces/CodeforcesUtils.kt
@@ -1,29 +1,13 @@
 package com.demich.cps.platforms.utils.codeforces
 
 import com.demich.cps.platforms.api.codeforces.CodeforcesPageContentProvider
-import com.demich.cps.platforms.utils.EvaluatorTagWithClass
-import com.demich.cps.platforms.utils.expectFirst
 import com.demich.cps.platforms.utils.parseDocument
 import com.demich.cps.platforms.utils.parseHtmlElement
 import org.jsoup.nodes.Element
 import org.jsoup.select.Evaluator
 
-private val evaluatorDivInfo = EvaluatorTagWithClass(tag = "div", className = "info")
-private fun Element.expectDivInfo(): Element = expectFirst(evaluatorDivInfo)
-
 private val evaluatorRatedUser = Evaluator.Class("rated-user")
 private fun Element.selectRatedUser(): Element? = selectFirst(evaluatorRatedUser)
-private fun Element.expectRatedUser(): Element = expectFirst(evaluatorRatedUser)
-
-private val evaluatorHumanTime = Evaluator.Class("format-humantime")
-private fun Element.expectHumanTime(): Element = expectFirst(evaluatorHumanTime)
-
-private val evaluatorHrefBlogEntry = Evaluator.AttributeWithValueStarting("href", "/blog/entry/")
-
-
-object CodeforcesUtils: CodeforcesPageParser {
-
-}
 
 
 suspend fun CodeforcesPageContentProvider.getRealColorTagOrNull(handle: String): CodeforcesColorTag? {
