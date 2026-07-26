37ec707fb3082daa0803fb2c4e0f2845f00c3ac5
demidenko <devit15@yandex.ru>
2026-07-26 17:38:32 +0700

    move to fun (println concurrent mess)

--- cps-compose/data/platforms/clients/src/main/java/com/demich/cps/platforms/clients/UrlPrintPlugin.kt
+++ cps-compose/data/platforms/clients/src/main/java/com/demich/cps/platforms/clients/UrlPrintPlugin.kt
@@ -2,19 +2,24 @@ package com.demich.cps.platforms.clients
 
 import io.ktor.client.plugins.api.Send
 import io.ktor.client.plugins.api.createClientPlugin
+import io.ktor.client.request.HttpRequestBuilder
 
 internal val UrlPrintPlugin get() = createClientPlugin(name = "UrlPrintingPlugin") {
     on(Send) { request ->
-        with(request) {
-            println("sending request: ${url.buildString()}")
-            val parameters = url.parameters.build()
-            if (!parameters.isEmpty()) {
-                println("${url.pathSegments.joinToString(separator = "/")} parameters:")
-                parameters.forEach { key, values ->
-                    println("\t$key: ${if (values.size == 1) values[0] else values}")
-                }
-            }
-        }
+        println(request.urlMessage())
         proceed(request)
+    }
+}
+
+private fun HttpRequestBuilder.urlMessage(): String = buildString {
+    append("sending request: ${url.buildString()}")
+    val parameters = url.parameters.build()
+    if (!parameters.isEmpty()) {
+        appendLine()
+        append("${url.pathSegments.joinToString(separator = "/")} parameters:")
+        parameters.forEach { key, values ->
+            appendLine()
+            append("\t$key: ${if (values.size == 1) values[0] else values}")
+        }
     }
 }
