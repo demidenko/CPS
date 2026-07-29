659eba266af52825295801c4f3dec48fb58a5456
demidenko <devit15@yandex.ru>
2026-07-29 18:32:11 +0700

    move to file

--- cps-compose/app/src/main/java/com/demich/cps/utils/FetchState.kt
+++ cps-compose/app/src/main/java/com/demich/cps/utils/FetchState.kt
@@ -35,26 +35,6 @@ fun FetchState<*>.toLoadingStatus(): LoadingStatus =
         is FetchResult.Success -> PENDING
     }
 
-// TODO: better name and signature
-data class FetchValue<out T>(
-    val value: T,
-    val state: FetchState<T>
-) {
-    constructor(value: T): this(
-        value = value,
-        state = FetchResult.Success(value)
-    )
-}
-
-operator fun <T> FetchValue<T>.plus(state: FetchState<T>): FetchValue<T> =
-    when (state) {
-        is FetchResult.Success -> FetchValue(value = state.value, state = state)
-        is FetchResult.Failure, is FetchState.Loading -> copy(state = state)
-    }
-
-val FetchValue<*>.loadingStatus: LoadingStatus
-    get() = state.toLoadingStatus()
-
 fun <T> Flow<T>.toFetchResultFlow(): Flow<FetchResult<T>> =
     map<T, FetchResult<T>> { FetchResult.Success(it) }
         .catch {
---
+++ cps-compose/app/src/main/java/com/demich/cps/utils/FetchValue.kt
@@ -0,0 +1,21 @@
+package com.demich.cps.utils
+
+// TODO: better name and signature
+data class FetchValue<out T>(
+    val value: T,
+    val state: FetchState<T>
+) {
+    constructor(value: T): this(
+        value = value,
+        state = FetchResult.Success(value)
+    )
+}
+
+operator fun <T> FetchValue<T>.plus(state: FetchState<T>): FetchValue<T> =
+    when (state) {
+        is FetchResult.Success -> FetchValue(value = state.value, state = state)
+        is FetchResult.Failure, is FetchState.Loading -> copy(state = state)
+    }
+
+val FetchValue<*>.loadingStatus: LoadingStatus
+    get() = state.toLoadingStatus()
