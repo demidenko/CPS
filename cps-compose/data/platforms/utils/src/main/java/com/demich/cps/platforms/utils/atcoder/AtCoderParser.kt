4892f5b80a79b3782192eaa249bc9ed023e3d99a
demidenko <devit15@yandex.ru>
2026-08-03 03:03:25 +0700

    optimize atcoder parser

--- cps-compose/data/platforms/utils/src/main/java/com/demich/cps/platforms/utils/atcoder/AtCoderParser.kt
+++ cps-compose/data/platforms/utils/src/main/java/com/demich/cps/platforms/utils/atcoder/AtCoderParser.kt
@@ -1,40 +1,54 @@
 package com.demich.cps.platforms.utils.atcoder
 
+import com.demich.cps.platforms.utils.EvaluatorTagWithClass
+import com.demich.cps.platforms.utils.EvaluatorTagWithId
 import com.demich.cps.platforms.utils.NewsPostEntry
+import com.demich.cps.platforms.utils.expectFirst
 import com.demich.cps.platforms.utils.href
 import com.demich.cps.platforms.utils.parseDocument
+import com.demich.cps.platforms.utils.selectSequence
 import com.demich.cps.profiles.userinfo.AtCoderUserInfo
 import com.demich.cps.profiles.userinfo.UserSuggestion
+import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
+import org.jsoup.select.Evaluator
 import kotlin.time.Instant
 
 class AtCoderParser {
-    fun extractUserInfo(source: String): AtCoderUserInfo =
-        with(source.parseDocument()) {
-            AtCoderUserInfo(
-                handle = expectFirst("a.username").text(),
-                rating = select("th.no-break").find { it.text() == "Rating" }
-                    ?.nextElementSibling()
-                    ?.selectFirst("span")
-                    ?.text()?.toInt()
-            )
-        }
+    private fun Document.expectUserMainContainer() =
+        expectFirst(EvaluatorTagWithId(tag = "div", id = "main-container"))
+
+    fun extractUserInfo(source: String): AtCoderUserInfo {
+        val container = source.parseDocument().expectUserMainContainer()
+        val handle = container.expectFirst(Evaluator.Class("username")).text()
+        val rating = container.selectSequence(EvaluatorTagWithClass(tag = "th", className = "no-break"))
+            .find { it.text() == "Rating" }
+            ?.nextElementSibling()
+            ?.selectFirst("span")
+            ?.text()?.toInt()
+
+        return AtCoderUserInfo(
+            handle = handle,
+            rating = rating
+        )
+    }
 
     fun extractUserSuggestions(source: String): List<UserSuggestion> {
         val table = source.parseDocument().expectFirst("table.table")
         val ratingIndex = table.select("thead > tr > th").indexOfFirst { it.text() == "Rating" }
-        return table.select("tbody > tr").map { row ->
+        return table.selectSequence("tbody > tr").map { row ->
             UserSuggestion(
                 userId = row.expectFirst("a.username").text(),
                 info = row.select("td")[ratingIndex].text()
             )
-        }
+        }.toList()
     }
 
     fun extractNews(source: String): List<AtcoderNewsPost?> =
         source.parseDocument()
-            .select("div.panel.panel-default, div.panel.panel-info")
+            .selectSequence("div.panel.panel-default, div.panel.panel-info")
             .mapNotNull { it.extractNewsFromPanel() }
+            .toList()
             .sortedByDescending { it.time }
 
     private fun Element.extractNewsFromPanel(): AtcoderNewsPost? {
