38dd593c5e84a36de59fa9474f544b82d2620386
demidenko <devit15@yandex.ru>
2026-08-07 19:15:31 +0700

    ++build

--- cps-compose/app/build.gradle.kts
+++ cps-compose/app/build.gradle.kts
@@ -12,7 +12,7 @@ configure<ApplicationExtension> {
     defaultConfig {
         applicationId = "com.demich.cps"
 
-        versionCode = 505
+        versionCode = 506
         versionName = "1.9.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
