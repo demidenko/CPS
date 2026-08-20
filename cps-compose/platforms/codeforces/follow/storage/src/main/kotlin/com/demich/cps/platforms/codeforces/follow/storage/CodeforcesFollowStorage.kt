05e1d076be06ab64d57cb2f51481442f3cf5dc0d
demidenko <devit15@yandex.ru>
2026-08-20 11:13:17 +0700

    hard

--- cps-compose/platforms/codeforces/follow/storage/src/main/kotlin/com/demich/cps/platforms/codeforces/follow/storage/CodeforcesFollowStorage.kt
+++ cps-compose/platforms/codeforces/follow/storage/src/main/kotlin/com/demich/cps/platforms/codeforces/follow/storage/CodeforcesFollowStorage.kt
@@ -1,16 +1,29 @@
 package com.demich.cps.platforms.codeforces.follow.storage
 
+import com.demich.cps.profiles.userinfo.CodeforcesUserInfo
+import com.demich.cps.profiles.userinfo.ProfileResult
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 
-interface CodeforcesFollowStorage {
-
-    suspend fun blogs(): List<CodeforcesUserBlog>
+private interface CodeforcesFollowStorage {
+    //read
 
     fun flowOfUserBlogs(): Flow<List<CodeforcesUserBlog>>
 
-    suspend fun blogOrNull(blogId: Long): CodeforcesUserBlog?
+    suspend fun userBlogs(): List<CodeforcesUserBlog> =
+        flowOfUserBlogs().first()
 
     fun flowOfUserBlog(blogId: Long): Flow<CodeforcesUserBlog?>
 
+    suspend fun userBlogOrNull(blogId: Long): CodeforcesUserBlog? =
+        flowOfUserBlog(blogId = blogId).first()
+
+    suspend fun userHandles(): List<String> =
+        userBlogs().map { it.handle }
+
+    // write
+
     suspend fun remove(blogId: Long)
+
+    suspend fun createUserWithoutBlog(profileResult: ProfileResult<CodeforcesUserInfo>): Boolean
 }
