1484bce6bee87c1ddce676da0105ca7702e07b8d
demidenko <devit15@yandex.ru>
2026-09-21 02:00:19 +0700

    add kotlinx-collections-immutable

--- cps-compose/app/build.gradle.kts
+++ cps-compose/app/build.gradle.kts
@@ -81,6 +81,7 @@ dependencies {
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.kotlinx.datetime)
     implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.kotlinx.collections.immutable)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.work.runtime)
